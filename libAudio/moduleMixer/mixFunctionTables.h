210d2d10ea1d459b22e2eaf9389574d88ad84782
Rachel Mant <dx-mon@users.sourceforge.net>
2011-12-29 03:15:59 +0000

    Implemented the rest of the 16-bit mixer functions and made some tidy-ups in the mixer routines

--- libAudio/moduleMixer/mixFunctionTables.h
+++ libAudio/moduleMixer/mixFunctionTables.h
@@ -8,18 +8,31 @@
 #define MIX_KAISERSRC		0x06
 #define MIX_FIRFILTERSRC	0x08
 #define MIX_FILTER			0x10
+#define MIX_16BIT			0x20
 
-MixInterface MixFunctionTable[32] = 
+MixInterface MixFunctionTable[64] = 
 {
-	// Non ramping functions
+	//// 8-bit ////
+	// Non filtering functions
 	Mono8BitMix, Mono8BitRampMix, Mono8BitLinearMix, Mono8BitLinearRampMix,
-	Mono8BitHQMix, Mono8BitLinearMix, Mono8BitKaiserMix, Mono8BitKaiserMix,
-	Mono8BitFIRFilterMix, Mono8BitFIRFilterMix, NULL, NULL,
+	Mono8BitHQMix, Mono8BitHQRampMix, Mono8BitKaiserMix, Mono8BitKaiserRampMix,
+	Mono8BitFIRFilterMix, Mono8BitFIRFilterRampMix, NULL, NULL,
 	NULL, NULL, NULL, NULL,
-	// Ramping functions
+	// Filtering functions
 	FilterMono8BitMix, FilterMono8BitRampMix, FilterMono8BitLinearMix, FilterMono8BitLinearRampMix,
 	FilterMono8BitHQRampMix, FilterMono8BitLinearRampMix, FilterMono8BitKaiserRampMix, FilterMono8BitKaiserRampMix,
 	FilterMono8BitFIRFilterRampMix, FilterMono8BitFIRFilterRampMix, NULL, NULL,
+	NULL, NULL, NULL, NULL,
+	//// 16-bit ////
+	// Non filtering functions
+	Mono16BitMix, Mono16BitRampMix, Mono16BitLinearMix, Mono16BitLinearRampMix,
+	Mono16BitHQMix, Mono16BitHQRampMix, Mono16BitKaiserMix, Mono16BitKaiserRampMix,
+	Mono16BitFIRFilterMix, Mono16BitFIRFilterRampMix, NULL, NULL,
+	NULL, NULL, NULL, NULL,
+	// Filtering functions
+	FilterMono16BitMix, FilterMono16BitRampMix, FilterMono16BitLinearMix, FilterMono16BitLinearRampMix,
+	FilterMono16BitHQRampMix, FilterMono16BitLinearRampMix, FilterMono16BitKaiserRampMix, FilterMono16BitKaiserRampMix,
+	FilterMono16BitFIRFilterRampMix, FilterMono16BitFIRFilterRampMix, NULL, NULL,
 	NULL, NULL, NULL, NULL,
 };
 
