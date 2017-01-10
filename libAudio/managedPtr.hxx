b263676d7f6ac17f635fafd97dde7de1e776dc0a
dx-mon <dx-mon@users.sourceforge.net>
2017-01-10 23:49:31 +0000

    Swapped in a new implementation of managedPtr_t<> ready to begin cleanup and improving the old

--- libAudio/managedPtr.hxx
+++ libAudio/managedPtr.hxx
@@ -98,7 +98,101 @@ namespace managedPtr
 	};
 }
 
-template<typename T> using managedPtr_t = managedPtr::managedPtr_t<T>;
+//template<typename T> using managedPtr_t = managedPtr::managedPtr_t<T>;
 //using managedPtr::managedPtr_t<void>;
+
+template<typename T> struct managedPtr_t
+{
+private:
+	using delete_t = void (*)(void *const);
+
+	T *ptr;
+	delete_t deleteFunc;
+	friend struct managedPtr_t<void>;
+
+	template<typename U = T> static void del(void *const object)
+	{
+		if (object)
+			static_cast<U *>(object)->~U();
+		operator delete(object);
+	}
+
+public:
+	using pointer = T *;
+	using reference = T &;
+
+	constexpr managedPtr_t() noexcept : ptr(nullptr), deleteFunc(nullptr) { }
+	managedPtr_t(T *p) noexcept : ptr(p), deleteFunc(del<T>) { }
+	managedPtr_t(managedPtr_t &&p) noexcept : managedPtr_t() { swap(p); }
+	~managedPtr_t() noexcept { if (deleteFunc) deleteFunc(ptr); }
+	managedPtr_t &operator =(managedPtr_t &&p) noexcept { swap(p); return *this; }
+
+	operator T &() const noexcept { return *ptr; }
+	explicit operator T &&() const = delete;
+	T &operator *() const noexcept { return *ptr; }
+	T *operator ->() const noexcept { return ptr; }
+	T *get() noexcept { return ptr; }
+	T *get() const noexcept { return ptr; }
+	explicit operator bool() const noexcept { return ptr; }
+
+	void swap(managedPtr_t &p) noexcept
+	{
+		std::swap(ptr, p.ptr);
+		std::swap(deleteFunc, p.deleteFunc);
+	}
+
+	managedPtr_t(const managedPtr_t &) = delete;
+	managedPtr_t &operator =(const managedPtr_t &) = delete;
+};
+
+template<> struct managedPtr_t<void>
+{
+private:
+	using delete_t = void (*)(void *const);
+
+	void *ptr;
+	delete_t deleteFunc;
+
+public:
+	constexpr managedPtr_t() noexcept : ptr(nullptr), deleteFunc(nullptr) { }
+	managedPtr_t(managedPtr_t &&p) noexcept : managedPtr_t() { swap(p); }
+	template<typename T, typename = typename std::enable_if<!std::is_same<T, void>::value>::type>
+		managedPtr_t(managedPtr_t<T> &&p) noexcept : managedPtr_t() { swap(p); }
+	~managedPtr_t() noexcept { if (deleteFunc) deleteFunc(ptr); }
+	managedPtr_t &operator =(managedPtr_t &&p) noexcept { swap(p); return *this; }
+	template<typename T> managedPtr_t &operator =(T *obj) noexcept { return *this = managedPtr_t<T>(obj); }
+	template<typename T, typename = typename std::enable_if<!std::is_same<T, void>::value>::type>
+		managedPtr_t &operator =(managedPtr_t<T> &&p) noexcept { swap(p); return *this; }
+
+	operator const void *() const noexcept { return ptr; }
+	operator void *() noexcept { return ptr; }
+	void *get() noexcept { return ptr; }
+	template<typename T> T *get() noexcept { return reinterpret_cast<T *>(ptr); }
+	void *get() const noexcept { return ptr; }
+	template<typename T> T *get() const noexcept { return reinterpret_cast<T *const>(ptr); }
+	explicit operator bool() const noexcept { return ptr; }
+
+	void swap(managedPtr_t &p) noexcept
+	{
+		std::swap(ptr, p.ptr);
+		std::swap(deleteFunc, p.deleteFunc);
+	}
+
+	template<typename T, typename = typename std::enable_if<!std::is_same<T, void>::value>::type>
+		void swap(managedPtr_t<T> &p) noexcept
+	{
+		if (deleteFunc)
+			deleteFunc(ptr);
+		ptr = p.ptr;
+		deleteFunc = p.deleteFunc;
+		p.ptr = nullptr;
+		p.deleteFunc = nullptr;
+	}
+
+	managedPtr_t(void *) = delete;
+	managedPtr_t(const managedPtr_t &) = delete;
+	managedPtr_t &operator =(void *) = delete;
+	managedPtr_t &operator =(const managedPtr_t &) = delete;
+};
 
 #endif /*MANAGED_PTR__HXX*/
