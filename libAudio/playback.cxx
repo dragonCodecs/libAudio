8c29b86289c7cbdc3967b3b52bd7beab4eb47f89
dx-mon <dx-mon@users.sourceforge.net>
2019-02-15 20:47:51 +0000

    More playback_t infrastructure

--- libAudio/playback.cxx
+++ libAudio/playback.cxx
@@ -1,17 +1,38 @@
 #include "playback.hxx"
+#include "openALPlayback.hxx"
+
+using player_t = openALPlayback_t;
 
 playback_t::playback_t(void *const audioFile_, const bufferFillFunc_t fillBuffer_, uint8_t *const buffer_,
 	const uint32_t bufferLength_, const fileInfo_t &fileInfo) : audioFile{audioFile_}, fillBuffer{fillBuffer_},
-	buffer{buffer_}, bufferLength{bufferLength_}, bitRate{fileInfo.bitRate}, channels{fileInfo.channels} { }
+	buffer{buffer_}, bufferLength{bufferLength_}, bitRate{fileInfo.bitRate}, channels{fileInfo.channels},
+	player{makeOpaque<player_t>(*this)} { }
 
 void playback_t::play()
 {
+	if (player)
+		player->play();
 }
 
 void playback_t::pause()
 {
+	if (player)
+		player->pause();
 }
 
 void playback_t::stop()
 {
+	if (player)
+		player->stop();
 }
+
+long audioPlayer_t::refillBuffer() const noexcept
+	{ return player.refillBuffer(); }
+long playback_t::refillBuffer() noexcept
+	{ return fillBuffer(audioFile, buffer, bufferLength); }
+
+uint8_t *audioPlayer_t::buffer() const noexcept { return player.buffer; }
+uint32_t audioPlayer_t::bufferLength() const noexcept { return player.bufferLength; }
+uint8_t audioPlayer_t::bitsPerSample() const noexcept { return player.bitsPerSample; }
+uint32_t audioPlayer_t::bitRate() const noexcept { return player.bitRate; }
+uint8_t audioPlayer_t::channels() const noexcept { return player.channels; }
--- libAudio/playback.hxx
+++ libAudio/playback.hxx
@@ -4,8 +4,31 @@
 #include <stdint.h>
 #include "libAudio.h"
 #include "fileInfo.hxx"
+#include "opaquePtr.hxx"
 
 using bufferFillFunc_t = long (*)(void *p_File, uint8_t *OutBuffer, int nOutBufferLen);
+
+struct playback_t;
+struct audioPlayer_t
+{
+private:
+	playback_t &player;
+
+protected:
+	audioPlayer_t(playback_t &_player) noexcept : player{_player} { }
+	long refillBuffer() const noexcept;
+	uint8_t *buffer() const noexcept;
+	uint32_t bufferLength() const noexcept;
+	uint8_t bitsPerSample() const noexcept;
+	uint32_t bitRate() const noexcept;
+	uint8_t channels() const noexcept;
+
+public:
+	virtual ~audioPlayer_t() { }
+	virtual void play() = 0;
+	virtual void pause() = 0;
+	virtual void stop() = 0;
+};
 
 struct playback_t final
 {
@@ -14,8 +37,14 @@ private:
 	bufferFillFunc_t fillBuffer;
 	uint8_t *buffer;
 	uint32_t bufferLength;
+	uint8_t bitsPerSample;
 	uint32_t bitRate;
 	uint8_t channels;
+	opaquePtr_t<audioPlayer_t> player;
+
+protected:
+	long refillBuffer() noexcept;
+	friend struct audioPlayer_t;
 
 public:
 	playback_t(void *const audioFile, const bufferFillFunc_t fillBuffer, uint8_t *const buffer,
