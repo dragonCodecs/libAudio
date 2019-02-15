042772fe7c726c606129149ad2f77969205d9918
dx-mon <dx-mon@users.sourceforge.net>
2019-02-15 21:49:43 +0000

    Added playback modes and state to playback_t and audioPlayer_t

--- libAudio/playback.cxx
+++ libAudio/playback.cxx
@@ -6,7 +6,7 @@ using player_t = openALPlayback_t;
 playback_t::playback_t(void *const audioFile_, const bufferFillFunc_t fillBuffer_, uint8_t *const buffer_,
 	const uint32_t bufferLength_, const fileInfo_t &fileInfo) : audioFile{audioFile_}, fillBuffer{fillBuffer_},
 	buffer{buffer_}, bufferLength{bufferLength_}, bitRate{fileInfo.bitRate}, channels{fileInfo.channels},
-	player{makeOpaque<player_t>(*this)} { }
+	playbackMode{playbackMode_t::wait}, player{makeOpaque<player_t>(*this)} { }
 
 void playback_t::play()
 {
@@ -30,9 +30,12 @@ long audioPlayer_t::refillBuffer() const noexcept
 	{ return player.refillBuffer(); }
 long playback_t::refillBuffer() noexcept
 	{ return fillBuffer(audioFile, buffer, bufferLength); }
+void playback_t::mode(playbackMode_t _mode) noexcept { playbackMode = _mode; }
 
 uint8_t *audioPlayer_t::buffer() const noexcept { return player.buffer; }
 uint32_t audioPlayer_t::bufferLength() const noexcept { return player.bufferLength; }
 uint8_t audioPlayer_t::bitsPerSample() const noexcept { return player.bitsPerSample; }
 uint32_t audioPlayer_t::bitRate() const noexcept { return player.bitRate; }
 uint8_t audioPlayer_t::channels() const noexcept { return player.channels; }
+bool audioPlayer_t::isPlaying() const noexcept { return state == playState_t::playing; }
+playbackMode_t audioPlayer_t::mode() const noexcept { return player.playbackMode; }
--- libAudio/playback.hxx
+++ libAudio/playback.hxx
@@ -1,12 +1,25 @@
 #ifndef PLAYBACK__HXX
 #define PLAYBACK__HXX
 
+#include <mutex>
 #include <stdint.h>
 #include "libAudio.h"
 #include "fileInfo.hxx"
 #include "opaquePtr.hxx"
 
 using bufferFillFunc_t = long (*)(void *p_File, uint8_t *OutBuffer, int nOutBufferLen);
+
+enum class playState_t
+{
+	stop,
+	stopped,
+	playing,
+	pause,
+	paused
+};
+
+enum class playbackMode_t
+	{ wait, async };
 
 struct playback_t;
 struct audioPlayer_t
@@ -15,13 +28,19 @@ private:
 	playback_t &player;
 
 protected:
-	audioPlayer_t(playback_t &_player) noexcept : player{_player} { }
+	playState_t state;
+	std::mutex stateMutex;
+
+	audioPlayer_t(playback_t &_player) noexcept : player{_player},
+		state{playState_t::stopped}, stateMutex{} { }
 	long refillBuffer() const noexcept;
 	uint8_t *buffer() const noexcept;
 	uint32_t bufferLength() const noexcept;
 	uint8_t bitsPerSample() const noexcept;
 	uint32_t bitRate() const noexcept;
 	uint8_t channels() const noexcept;
+	playbackMode_t mode() const noexcept;
+	bool isPlaying() const noexcept;
 
 public:
 	virtual ~audioPlayer_t() { }
@@ -40,6 +59,7 @@ private:
 	uint8_t bitsPerSample;
 	uint32_t bitRate;
 	uint8_t channels;
+	playbackMode_t playbackMode;
 	opaquePtr_t<audioPlayer_t> player;
 
 protected:
@@ -49,6 +69,7 @@ protected:
 public:
 	playback_t(void *const audioFile, const bufferFillFunc_t fillBuffer, uint8_t *const buffer,
 		const uint32_t bufferLength, const fileInfo_t &fileInfo);
+	void mode(playbackMode_t mode) noexcept;
 	void play();
 	void pause();
 	void stop();
