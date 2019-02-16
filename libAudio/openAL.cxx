280e4739f79039bb4ba06c08ba8183796476cf06
dx-mon <dx-mon@users.sourceforge.net>
2019-02-16 14:58:03 +0000

    Shimmed all OpenAL calls to allow error reporting (even if that is the only action) for all OpenAL actions

--- libAudio/openAL.cxx
+++ libAudio/openAL.cxx
@@ -1,4 +1,5 @@
 #include "openAL.hxx"
+#include "openALShim.hxx"
 
 std::unique_ptr<alContext_t> alContext;
 
@@ -15,83 +16,83 @@ alContext_t::alContext_t() noexcept : device{alcOpenDevice(alcGetString(nullptr,
 
 alContext_t::~alContext_t()
 {
-	if (alcGetCurrentContext() == context)
-		alcMakeContextCurrent(nullptr);
-	alcDestroyContext(context);
-	alcCloseDevice(device);
+	if (al::alcGetCurrentContext() == context)
+		al::alcMakeContextCurrent(nullptr);
+	al::alcDestroyContext(context);
+	al::alcCloseDevice(device);
 }
 
 void alContext_t::makeCurrent() noexcept
-	{ alcMakeContextCurrent(context); }
+	{ al::alcMakeContextCurrent(context); }
 
 alSource_t::alSource_t() noexcept : source{AL_NONE}
 {
-	alGenSources(1, &source);
+	al::alGenSources(1, &source);
 	if (!source)
 		return;
-	alSourcef(source, AL_GAIN, 1);
-	alSourcef(source, AL_PITCH, 1);
-	alSource3f(source, AL_POSITION, 0, 0, 0);
-	alSource3f(source, AL_VELOCITY, 0, 0, 0);
-	alSource3f(source, AL_DIRECTION, 0, 0, 0);
-	alSourcef(source, AL_ROLLOFF_FACTOR, 0);
+	al::alSourcef(source, AL_GAIN, 1);
+	al::alSourcef(source, AL_PITCH, 1);
+	al::alSource3f(source, AL_POSITION, 0, 0, 0);
+	al::alSource3f(source, AL_VELOCITY, 0, 0, 0);
+	al::alSource3f(source, AL_DIRECTION, 0, 0, 0);
+	al::alSourcef(source, AL_ROLLOFF_FACTOR, 0);
 }
 
 alSource_t::~alSource_t() noexcept
 {
 	if (source != AL_NONE)
-		alDeleteSources(1, &source);
+		al::alDeleteSources(1, &source);
 }
 
 void alSource_t::queue(alBuffer_t &buffer) const noexcept
 {
 	ALuint _buffer = buffer;
-	alSourceQueueBuffers(source, 1, &_buffer);
+	al::alSourceQueueBuffers(source, 1, &_buffer);
 	buffer.isQueued(true);
 }
 
 ALuint alSource_t::dequeueOne() const noexcept
 {
 	ALuint buffer = AL_NONE;
-	alSourceUnqueueBuffers(source, 1, &buffer);
+	al::alSourceUnqueueBuffers(source, 1, &buffer);
 	return buffer;
 }
 
-void alSource_t::play() const noexcept { alSourcePlay(source); }
-void alSource_t::pause() const noexcept { alSourcePause(source); }
-void alSource_t::stop() const noexcept { alSourceStop(source); }
+void alSource_t::play() const noexcept { al::alSourcePlay(source); }
+void alSource_t::pause() const noexcept { al::alSourcePause(source); }
+void alSource_t::stop() const noexcept { al::alSourceStop(source); }
 
 int alSource_t::processedBuffers() const noexcept
 {
 	int result = 0;
-	alGetSourcei(source, AL_BUFFERS_PROCESSED, &result);
+	al::alGetSourcei(source, AL_BUFFERS_PROCESSED, &result);
 	return result;
 }
 
 int alSource_t::queuedBuffers() const noexcept
 {
 	int result = 0;
-	alGetSourcei(source, AL_BUFFERS_QUEUED, &result);
+	al::alGetSourcei(source, AL_BUFFERS_QUEUED, &result);
 	return result;
 }
 
 int alSource_t::state() const noexcept
 {
 	int result = 0;
-	alGetSourcei(source, AL_SOURCE_STATE, &result);
+	al::alGetSourcei(source, AL_SOURCE_STATE, &result);
 	return result;
 }
 
 alBuffer_t::alBuffer_t() noexcept : buffer{AL_NONE}, queued{false}
-	{ alGenBuffers(1, &buffer); }
+	{ al::alGenBuffers(1, &buffer); }
 
 alBuffer_t::~alBuffer_t() noexcept
 {
 	if (buffer != AL_NONE)
-		alDeleteBuffers(1, &buffer);
+		al::alDeleteBuffers(1, &buffer);
 }
 
 bool alBuffer_t::operator ==(const ALuint value) const noexcept
 	{ return buffer == value; }
 void alBuffer_t::fill(const void *const data, const uint32_t dataLength, const ALenum format,
-	uint32_t frequency) const noexcept { alBufferData(buffer, format, data, dataLength, frequency); }
+	uint32_t frequency) const noexcept { al::alBufferData(buffer, format, data, dataLength, frequency); }
---
+++ libAudio/openALShim.hxx
@@ -0,0 +1,80 @@
+#include <utility>
+
+inline const char *alErrorString(const ALenum error) noexcept
+{
+	switch (error)
+	{
+		case AL_NO_ERROR:
+			return "No error";
+		case AL_INVALID_NAME:
+			return "Invalid parameter name given";
+		case AL_INVALID_ENUM:
+			return "Invalid enumeration value";
+		case AL_INVALID_VALUE:
+			return "Invalid value - out of range";
+		case AL_INVALID_OPERATION:
+			return "Invalid operation";
+		case AL_OUT_OF_MEMORY:
+			return "Out of memory";
+		default:
+			return "Unknown";
+	}
+}
+
+using std::declval;
+
+template<typename function_t> struct alCall_t
+{
+	function_t function;
+	const char *const functionName;
+
+	template<typename... args_t, typename result_t = decltype(declval<function_t>()(declval<args_t>()...)),
+		typename = typename std::enable_if<!std::is_same<result_t, void>::value>::type>
+		auto operator ()(args_t &&... args) const noexcept -> result_t
+	{
+		auto result = function(std::forward<args_t>(args)...);
+		const auto error = alGetError();
+		if (error != AL_NO_ERROR)
+			fprintf(stderr, "libAudio: %s() - %s\n", functionName, alErrorString(error));
+		return result;
+	}
+
+	template<typename... args_t, typename result_t = decltype(declval<function_t>()(declval<args_t>()...)),
+		typename = typename std::enable_if<std::is_same<result_t, void>::value>::type>
+		void operator ()(args_t &&... args) const noexcept
+	{
+		function(std::forward<args_t>(args)...);
+		const auto error = alGetError();
+		if (error != AL_NO_ERROR)
+			fprintf(stderr, "libAudio: %s() - %s\n", functionName, alErrorString(error));
+	}
+};
+
+#define AL_CALL(function) alCall_t<decltype((function))>{function, #function}
+
+namespace al
+{
+	auto alcGetString = AL_CALL(::alcGetString);
+	auto alcOpenDevice = AL_CALL(::alcOpenDevice);
+	auto alcCreateContext = AL_CALL(::alcCreateContext);
+	auto alcGetCurrentContext = AL_CALL(::alcGetCurrentContext);
+	auto alcMakeContextCurrent = AL_CALL(::alcMakeContextCurrent);
+	auto alcDestroyContext = AL_CALL(::alcDestroyContext);
+	auto alcCloseDevice = AL_CALL(::alcCloseDevice);
+	auto alGenSources = AL_CALL(::alGenSources);
+	auto alSourcef = AL_CALL(::alSourcef);
+	auto alSource3f = AL_CALL(::alSource3f);
+	auto alListener3f = AL_CALL(::alListener3f);
+	auto alDeleteSources = AL_CALL(::alDeleteSources);
+	auto alSourceQueueBuffers = AL_CALL(::alSourceQueueBuffers);
+	auto alSourceUnqueueBuffers = AL_CALL(::alSourceUnqueueBuffers);
+	auto alSourcePlay = AL_CALL(::alSourcePlay);
+	auto alSourcePause = AL_CALL(::alSourcePause);
+	auto alSourceStop = AL_CALL(::alSourceStop);
+	auto alGetSourcei = AL_CALL(::alGetSourcei);
+	auto alGenBuffers = AL_CALL(::alGenBuffers);
+	auto alDeleteBuffers = AL_CALL(::alDeleteBuffers);
+	auto alBufferData = AL_CALL(::alBufferData);
+}
+
+#undef AL_CALL
