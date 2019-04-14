d8ac66e1c9ea05fb322b67e8308339788d58b256
dx-mon <dx-mon@users.sourceforge.net>
2019-04-14 17:56:45 -0400

    Improvements to the last commit to shut older GCC's up over the Clang attribute and enable newer GCCs to supress the same warning
    The clang attribute is technically non-complient.. but that's what they chose, so we get to lump it.

--- libAudio/moduleMixer/moduleMixer.cpp
+++ libAudio/moduleMixer/moduleMixer.cpp
@@ -449,7 +449,7 @@ void ModuleFile::HandleNNA(Channel *channel, uint32_t nSample, uint8_t note)
 					break;
 				case NNA_NOTECUT:
 					nnaChannel->FadeOutVol = 0;
-					[[clang::fallthrough]];
+					[[clang::fallthrough]]; // Falls through
 				case NNA_NOTEFADE:
 					nnaChannel->Flags |= CHN_NOTEFADE;
 					break;
