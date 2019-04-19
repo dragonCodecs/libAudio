9de5f99372548aa09dfa5e064a230bec2e769915
dx-mon <dx-mon@users.sourceforge.net>
2019-04-18 23:56:18 -0400

    Removed the old-style control structure for saveOggVorbis

--- libAudio/saveOggVorbis.cpp
+++ libAudio/saveOggVorbis.cpp
@@ -9,14 +9,6 @@
  * @date 2010-2019
  */
 
-typedef struct _OV_Intern
-{
-	oggVorbis_t inner;
-
-	_OV_Intern(const char *const fileName) noexcept :
-		inner(fd_t(fileName, O_RDWR | O_CREAT | O_TRUNC, normalMode), audioModeWrite_t{}) { }
-} OV_Intern;
-
 oggVorbis_t::oggVorbis_t(fd_t &&fd, audioModeWrite_t) noexcept :
 	audioFile_t(audioType_t::oggVorbis, std::move(fd)), encoderCtx(makeUnique<encoderContext_t>()) { }
 oggVorbis_t::encoderContext_t::encoderContext_t() noexcept : encoderState{}, blockState{},
@@ -27,19 +19,22 @@ oggVorbis_t::encoderContext_t::encoderContext_t() noexcept : encoderState{}, blo
 	ogg_stream_init(&streamState, randomDev());
 }
 
+oggVorbis_t *oggVorbis_t::openW(const char *const fileName) noexcept
+{
+	auto ovFile = makeUnique<oggVorbis_t>(fd_t(fileName, O_RDWR | O_CREAT | O_TRUNC, normalMode),
+		audioModeWrite_t{});
+	if (!ovFile || !ovFile->valid())
+		return nullptr;
+	return ovFile.release();
+}
+
 /*!
  * This function opens the file given by \c FileName for writing and returns a pointer
  * to the context of the opened file which must be used only by OggVorbis_* functions
  * @param FileName The name of the file to open
  * @return A void pointer to the context of the opened file, or \c NULL if there was an error
  */
-void *OggVorbis_OpenW(const char *FileName)
-{
-	std::unique_ptr<OV_Intern> ret = makeUnique<OV_Intern>(FileName);
-	if (!ret || !ret->inner.encoderContext())
-		return nullptr;
-	return ret.release();
-}
+void *OggVorbis_OpenW(const char *FileName) { return oggVorbis_t::openW(FileName); }
 
 bool oggVorbis_t::encoderContext_t::writePage(const fd_t &fd, const bool force) noexcept
 {
@@ -84,39 +79,35 @@ vorbis_comment copyComments(const FileInfo &info) noexcept
  * @bug \p p_VorbisFile must not be \c NULL as no checking on the parameter is done. FIXME!
  */
 bool OggVorbis_SetFileInfo(void *p_VorbisFile, FileInfo *p_FI)
+	{ return audioFileInfo(p_VorbisFile, p_FI); }
+
+bool oggVorbis_t::fileInfo(const FileInfo &fileInfo)
 {
-	OV_Intern *p_VF = (OV_Intern *)p_VorbisFile;
-	auto &ctx = *p_VF->inner.encoderContext();
-	fileInfo_t &info = p_VF->inner.fileInfo();
-	const fd_t &fd = p_VF->inner.fd();
+	auto &ctx = *encoderContext();
+	fileInfo_t &info = this->fileInfo();
 	ogg_packet packetHeader, packetComments, packetMode;
 
-	vorbis_encode_init_vbr(&ctx.vorbisInfo, p_FI->Channels, p_FI->BitRate, 0.75F);
+	vorbis_encode_init_vbr(&ctx.vorbisInfo, fileInfo.Channels, fileInfo.BitRate, 0.75F);
 	vorbis_encode_setup_init(&ctx.vorbisInfo);
 
 	vorbis_analysis_init(&ctx.encoderState, &ctx.vorbisInfo);
 	vorbis_block_init(&ctx.encoderState, &ctx.blockState);
 
-	vorbis_comment tags = copyComments(*p_FI);
+	vorbis_comment tags = copyComments(fileInfo);
 	vorbis_analysis_headerout(&ctx.encoderState, &tags, &packetHeader, &packetComments, &packetMode);
 	vorbis_comment_clear(&tags);
 	ogg_stream_packetin(&ctx.streamState, &packetHeader);
 	ogg_stream_packetin(&ctx.streamState, &packetComments);
 	ogg_stream_packetin(&ctx.streamState, &packetMode);
-	//ogg_packet_clear(&packetHeader);
-	//ogg_packet_clear(&packetComments);
-	//ogg_packet_clear(&packetMode);
 
-	if (!ctx.writePage(fd, true))
+	if (!ctx.writePage(fd(), true))
 		return false;
-	info.totalTime = p_FI->TotalTime;
-	info.bitsPerSample = p_FI->BitsPerSample;
-	info.bitRate = p_FI->BitRate;
-	info.channels = p_FI->Channels;
+	info.totalTime = fileInfo.TotalTime;
+	info.bitsPerSample = fileInfo.BitsPerSample;
+	info.bitRate = fileInfo.BitRate;
+	info.channels = fileInfo.Channels;
 	return true;
 }
-
-bool oggVorbis_t::fileInfo(const FileInfo &fileInfo) { return true; }
 
 /*!
  * This function writes a buffer of audio to a Ogg/Vorbis file opened being encoded
@@ -126,22 +117,23 @@ bool oggVorbis_t::fileInfo(const FileInfo &fileInfo) { return true; }
  * @attention Will not work unless \c OggVorbis_SetFileInfo() has been called beforehand
  */
 long OggVorbis_WriteBuffer(void *p_VorbisFile, uint8_t *InBuffer, int nInBufferLen)
-{
-	OV_Intern *p_VF = (OV_Intern *)p_VorbisFile;
-	fileInfo_t &info = p_VF->inner.fileInfo();
-	const fd_t &fd = p_VF->inner.fd();
-	auto &ctx = *p_VF->inner.encoderContext();
+	{ return audioWriteBuffer(p_VorbisFile, InBuffer, nInBufferLen); }
 
-	if (nInBufferLen <= 0)
+int64_t oggVorbis_t::writeBuffer(const void *const buffer, const uint32_t length)
+{
+	fileInfo_t &info = fileInfo();
+	auto &ctx = *encoderContext();
+
+	if (length <= 0)
 	{
 		vorbis_analysis_buffer(&ctx.encoderState, 0);
 		vorbis_analysis_wrote(&ctx.encoderState, 0);
 	}
 	else
 	{
-		uint32_t bufflen = (nInBufferLen / 2) / info.channels;
+		uint32_t bufflen = (length / 2) / info.channels;
 		float **buff = vorbis_analysis_buffer(&ctx.encoderState, bufflen);
-		short *IB = (short *)InBuffer;
+		const auto IB = static_cast<const int16_t *>(buffer);
 
 		for (uint32_t i = 0; i < bufflen; i++)
 		{
@@ -163,19 +155,17 @@ long OggVorbis_WriteBuffer(void *p_VorbisFile, uint8_t *InBuffer, int nInBufferL
 			ogg_stream_packetin(&ctx.streamState, &packet);
 			if (packet.bytes == 1)
 				continue; // Let's do this for speed, quicker than running the next while loop to find out!
-			else if (!ctx.writePage(fd, false))
+			else if (!ctx.writePage(fd(), false))
 				return -2;
 		}
 	}
 
 	if (!ctx.eos)
-		return nInBufferLen;
-	else if (!ctx.writePage(fd, true))
+		return length;
+	else if (!ctx.writePage(fd(), true))
 		return -2;
-	return nInBufferLen;
+	return length;
 }
-
-int64_t oggVorbis_t::writeBuffer(const void *const buffer, const uint32_t length) { return -1; }
 
 oggVorbis_t::encoderContext_t::~encoderContext_t() noexcept
 {
@@ -193,9 +183,4 @@ oggVorbis_t::encoderContext_t::~encoderContext_t() noexcept
  * this function - please either set it to \c NULL or be extra carefull
  * to destroy it via scope
  */
-int OggVorbis_CloseFileW(void *p_VorbisFile)
-{
-	OV_Intern *p_VF = (OV_Intern *)p_VorbisFile;
-	delete p_VF;
-	return 0;
-}
+int OggVorbis_CloseFileW(void *p_VorbisFile) { return audioCloseFile(p_VorbisFile); }
