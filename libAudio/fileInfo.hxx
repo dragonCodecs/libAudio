41799fff1814e50d7a5c512bff401e6de9c53b49
dx-mon <dx-mon@users.sourceforge.net>
2019-04-19 09:30:31 -0400

    Implemented an assignment operation for fileInfo_t that allows copying the non-string fields to other fileInfo_t instances

--- libAudio/fileInfo.hxx
+++ libAudio/fileInfo.hxx
@@ -17,8 +17,16 @@ struct fileInfo_t final
 	std::unique_ptr<char []> album;
 	std::vector<std::unique_ptr<char []>> other;
 
-	fileInfo_t() noexcept : totalTime(0), bitsPerSample(0), bitRate(0),
-		channels(0), title(), artist(), album(), other() { }
+	fileInfo_t() noexcept : totalTime{0}, bitsPerSample{0}, bitRate{0},
+		channels{0}, title{}, artist{}, album{}, other{} { }
+
+	void operator =(const fileInfo_t &info) noexcept
+	{
+		totalTime = info.totalTime;
+		bitsPerSample = info.bitsPerSample;
+		bitRate = info.bitRate;
+		channels = info.channels;
+	}
 };
 
 #endif /*FILE_INFO__HXX*/
