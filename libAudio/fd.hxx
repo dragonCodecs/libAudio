11db9297452766ce0d96e95c7541f9dafd89220d
dx-mon <dx-mon@users.sourceforge.net>
2019-05-01 22:31:47 +0100

    Cleanup and further improvements to fd_t

--- libAudio/fd.hxx
+++ libAudio/fd.hxx
@@ -4,8 +4,8 @@
 #include <stdint.h>
 #include <cstddef>
 #include <unistd.h>
-#include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <utility>
 #include <memory>
@@ -21,6 +21,8 @@
 #define WARN_UNUSED
 #endif
 
+using stat_t = struct stat;
+
 /*!
  * @internal
  * While this is supposed to be a very thin, RAII-only layer,
@@ -34,12 +36,13 @@ private:
 	mutable bool eof;
 
 public:
-	constexpr fd_t() noexcept : fd(-1), eof(false) { }
-	constexpr fd_t(const int32_t s) noexcept : fd(s), eof(false) { }
-	fd_t(const char *const file, int flags, mode_t mode = 0) noexcept : fd(::open(file, flags, mode)), eof(false) { }
-	fd_t(fd_t &&fd_) : fd_t() { swap(fd_); }
+	constexpr fd_t() noexcept : fd{-1}, eof{false} { }
+	constexpr fd_t(const int32_t fd_) noexcept : fd{fd_}, eof{false} { }
+	fd_t(const char *const file, const int flags, const mode_t mode = 0) noexcept :
+		fd{::open(file, flags, mode)}, eof{false} { }
+	fd_t(fd_t &&fd_) : fd_t{} { swap(fd_); }
 	~fd_t() noexcept { if (fd != -1) close(fd); }
-	void operator =(fd_t &&s) noexcept { swap(s); }
+	void operator =(fd_t &&fd_) noexcept { swap(fd_); }
 	operator int32_t() const noexcept WARN_UNUSED { return fd; }
 	bool operator ==(const int32_t desc) const noexcept WARN_UNUSED { return fd == desc; }
 	bool valid() const noexcept WARN_UNUSED { return fd != -1; }
@@ -48,23 +51,6 @@ public:
 	{
 		std::swap(fd, desc.fd);
 		std::swap(eof, desc.eof);
-	}
-
-	template<typename T> bool read(T &value) const noexcept
-		{ return read(&value, sizeof(T)); }
-	template<typename T, size_t N> bool read(std::array<T, N> &value) const noexcept
-		{ return read(value.data(), N * sizeof(T)); }
-	template<typename T> bool read(const std::unique_ptr<T> &value, const size_t valueLen) const noexcept
-		{ return read(value.get(), valueLen); }
-	template<typename T> bool read(const managedPtr_t<T> &value, const size_t valueLen) const noexcept
-		{ return read(value.get(), valueLen); }
-	bool read(const managedPtr_t<void> &value, const size_t valueLen) const noexcept WARN_UNUSED
-		{ return read(value.get(), valueLen); }
-
-	template<size_t length, typename T, size_t N> bool read(std::array<T, N> &value) const noexcept
-	{
-		static_assert(length <= N, "Can't request to read more than the std::array<> length");
-		return read(value.data(), length * sizeof(T));
 	}
 
 	bool read(void *const value, const size_t valueLen) const noexcept WARN_UNUSED
@@ -86,8 +72,10 @@ public:
 		return actualLen == valueLen;
 	}
 
-	ssize_t read(void *const bufferPtr, const size_t len, std::nullptr_t) const noexcept WARN_UNUSED { return ::read(fd, bufferPtr, len); }
-	ssize_t write(const void *const bufferPtr, const size_t len) const noexcept WARN_UNUSED { return ::write(fd, bufferPtr, len); }
+	ssize_t read(void *const bufferPtr, const size_t len, std::nullptr_t) const noexcept WARN_UNUSED
+		{ return ::read(fd, bufferPtr, len); }
+	ssize_t write(const void *const bufferPtr, const size_t len) const noexcept WARN_UNUSED
+		{ return ::write(fd, bufferPtr, len); }
 	off_t seek(off_t offset, int32_t whence) const noexcept WARN_UNUSED { return ::lseek(fd, offset, whence); }
 	off_t tell() const noexcept WARN_UNUSED { return seek(0, SEEK_CUR); }
 	fd_t dup() const noexcept WARN_UNUSED { return ::dup(fd); }
@@ -97,6 +85,25 @@ public:
 		struct stat fileStat;
 		const int result = fstat(fd, &fileStat);
 		return result ? -1 : fileStat.st_size;
+	}
+
+	template<typename T> bool read(T &value) const noexcept
+		{ return read(&value, sizeof(T)); }
+	template<typename T, size_t N> bool read(std::array<T, N> &value) const noexcept
+		{ return read(value.data(), N * sizeof(T)); }
+	template<typename T> bool read(const std::unique_ptr<T> &value) const noexcept
+		{ return read(value.get(), sizeof(T)); }
+	template<typename T> bool read(std::unique_ptr<T []> &value, const size_t valueCount) const noexcept
+		{ return read(value.get(), sizeof(T) * valueCount); }
+	template<typename T> bool read(const managedPtr_t<T> &value, const size_t valueLen) const noexcept
+		{ return read(value.get(), valueLen); }
+	bool read(const managedPtr_t<void> &value, const size_t valueLen) const noexcept WARN_UNUSED
+		{ return read(value.get(), valueLen); }
+
+	template<size_t length, typename T, size_t N> bool read(std::array<T, N> &value) const noexcept
+	{
+		static_assert(length <= N, "Can't request to read more than the std::array<> length");
+		return read(value.data(), sizeof(T) * length);
 	}
 
 	fd_t(const fd_t &) = delete;
