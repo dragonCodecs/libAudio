3e34cfaefdaf4c7091974851fcbebb863dd3fa52
dx-mon <dx-mon@users.sourceforge.net>
2019-05-03 01:22:18 +0100

    Added the meson and ninja main build invocation lines for Travis CI

--- .travis-ci/script.sh
+++ .travis-ci/script.sh
@@ -2,10 +2,23 @@
 export PS4="$ "
 set -x
 
+codecov()
+{
+	if [ $COVERAGE -ne 0 ]; then
+		echo true
+	else
+		echo false
+	fi
+}
+
+CC="$CC_" CXX="$CXX_" meson build --prefix=$HOME/.local -D b_coverage=`codecov`
 cd build
+ninja
 #if [ "$TRAVIS_OS_NAME" != "windows" ]; then
 #	ninja test
 #else
 #	meson test --no-rebuild --print-errorlogs -v
 #fi
 ninja install
+
+unset -f codecov
