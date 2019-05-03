6297d04fdc227fe5f7a96e7b94fb17b5e3cb7600
dx-mon <dx-mon@users.sourceforge.net>
2019-05-03 01:21:45 +0100

    Added building libmp4v2 to the Travis CI process

--- .travis-ci/install.sh
+++ .travis-ci/install.sh
@@ -6,5 +6,11 @@ if [ "$TRAVIS_OS_NAME" != "windows" ]; then
 	sudo apt-get install libopenal-dev libogg-dev libvorbis-dev \
 		libflac-dev libwavpack-dev libmpcdec-dev libfaac-dev libfaad-dev \
 		libmad0-dev libid3tag0-dev
+
+	pushd deps/mp4v2
+	CC=$CC_ CXX=$CXX_ ./configure --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu --disable-gch
+	make
+	sudo make install
+	popd
 else
 fi
