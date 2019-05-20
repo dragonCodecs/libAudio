f6f616ff88a4cc5a8d64809402d70acf0d178cf0
dx-mon <dx-mon@users.sourceforge.net>
2019-05-20 15:09:42 -0400

    Switched the player diagnostics to using the new output types

--- player/player.cxx
+++ player/player.cxx
@@ -1,10 +1,13 @@
 #include <libAudio.h>
-#include <stdio.h>
 #ifdef _WINDOWS
 //#include <process.h>
 //#include <windows.h>
 #endif
 #include <inttypes.h>
+#include "streamFormatter.hxx"
+
+file_t stdoutStream{stdout};
+streamFormatter_t out{stdoutStream};
 
 int main(int argc, char **argv)
 {
@@ -23,9 +26,11 @@ int main(int argc, char **argv)
 			continue;
 		}
 
-		printf("File %s, TotalTime: %" PRIu64 "m %us, BitRate: %uHz, Title: %s, Artist: %s, Album: %s, Channels: %d\n",
+		out.write("File ", argv[i], ", TotalTime: ", asTime_t{info->totalTime}, ", Sample Rate: ", info->bitRate, "Hz, Title: ",
+			info->title, ", Artist: ", info->artist, ", Album: ", info->album, ", Channels: ", info->channels, '\n');
+		/*printf("File %s, TotalTime: %" PRIu64 "m %us, BitRate: %uHz, Title: %s, Artist: %s, Album: %s, Channels: %d\n",
 			argv[i], info->totalTime / 60, uint8_t(info->totalTime % 60), info->bitRate, info->title.get(),
-			info->artist.get(), info->album.get(), info->channels);
+			info->artist.get(), info->album.get(), info->channels);*/
 		/*{ // The following code makes the UTF-8 returned in info display correctly under Windows.. the only OS that has a problem with this..
 			wchar_t *Title;
 			int wTitleLen = MultiByteToWideChar(CP_UTF8, 0, info->Title, -1, NULL, NULL);
