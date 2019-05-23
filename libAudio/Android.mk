595f4bda37b0d9194740395f30228e05d8c2daa5
dx-mon <dx-mon@users.sourceforge.net>
2019-05-23 20:01:03 +0100

    Fixed the Android build files up to reflect the changes made by rewriting the playback engine and making loadFC1x truely experimental (IE, not built by default)

--- libAudio/Android.mk
+++ libAudio/Android.mk
@@ -3,11 +3,14 @@ include $(call my-dir)/*/Android.mk
 SRC_WMA =
 #SRC_OPTIMFROG = loadOptimFROG.cpp
 SRC_OPTIMFROG =
-SRC_MOD = loadMOD.cpp loadS3M.cpp loadSTM.cpp loadAON.cpp loadFC1x.cpp loadIT.cpp
+#SRC_FC1x = loadFC1x.cpp
+SRC_FC1x =
+SRC_MOD = loadMOD.cpp loadS3M.cpp loadSTM.cpp loadAON.cpp $(SRC_FC1x) loadIT.cpp
 #SRC_SHDN = loadSHDN.cpp
 SRC_SHDN =
 SRC_SID = loadSID.cpp
-LOCAL_SRC_FILES += loadAudio.cpp libAudio_Common.cpp loadOggVorbis.cpp loadWAV.cpp loadAAC.cpp loadM4A.cpp loadMP3.cpp loadMPC.cpp loadFLAC.cpp $(SRC_MOD) $(SRC_SHDN) $(SRC_SID) \
-	loadWavPack.cpp $(SRC_OPTIMFROG) loadRealAudio.cpp $(SRC_WMA) saveAudio.cpp saveOggVorbis.cpp saveFLAC.cpp saveM4A.cpp
+LOCAL_SRC_FILES += loadAudio.cpp libAudio_Common.cpp loadOggVorbis.cpp loadWAV.cpp loadAAC.cpp loadM4A.cpp loadMP3.cpp loadMPC.cpp \
+	loadFLAC.cpp $(SRC_MOD) $(SRC_SHDN) $(SRC_SID) loadWavPack.cpp $(SRC_OPTIMFROG) loadRealAudio.cpp $(SRC_WMA) \
+	saveAudio.cpp saveOggVorbis.cpp saveFLAC.cpp saveM4A.cpp openAL.cxx openALPlayback.cxx playback.cxx
 
 LOCAL_SRC_FILES := $(addprefix libAudio/,$(LOCAL_SRC_FILES))
