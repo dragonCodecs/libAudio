87de1d5ed9f31eb1cdebdd10822fc55e550187ab
dx-mon <dx-mon@users.sourceforge.net>
2019-06-22 21:03:10 +0100

    Attempted to fix the MSYS2 package list once again

--- .travis-ci/beforeInstall.sh
+++ .travis-ci/beforeInstall.sh
@@ -14,7 +14,7 @@ else
 	wget --progress=dot:mega https://bootstrap.pypa.io/get-pip.py
 	wget --progress=dot:mega https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip
 	choco install -r --no-progress msys2 --params="'/NoUpdate'"
-	choco install -r --no-progress make
+#	choco install -r --no-progress make
 	choco install -r --no-progress python --version 3.6.8
 	python get-pip.py
 	pip3 install meson
@@ -23,7 +23,7 @@ else
 	rm get-pip.py ninja-win.zip
 
 	powershell -executionpolicy bypass "pacman -Sy --noconfirm"
-	powershell -executionpolicy bypass "pacman -S --noconfirm m4 autoconf autoconf-archive automake-wrapper libtool make pkg-config"
+	powershell -executionpolicy bypass "pacman -S --noconfirm m4 autoconf autoconf2.13 autoconf-archive automake-wrapper autogen libtool make pkg-config"
 
 	ln -sv /c/tools/msys64/usr/share/{autoconf,automake,aclocal,libtool,pkgconfig}* /usr/share
 	ln -sv /c/tools/msys64/usr/bin/{autom4te,autoconf,automake,autoheader,aclocal,libtool{,ize},m4} /usr/bin
