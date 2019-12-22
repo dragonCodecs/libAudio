1acdfcb23f1b01fda258728ad94ce688e2a93901
dx-mon <dx-mon@users.sourceforge.net>
2019-12-22 19:22:59 +0000

    Fixed up the documentation for loadMPCm making it bug- and parameter- accurate again

--- libAudio/loadMPC.cpp
+++ libAudio/loadMPC.cpp
@@ -9,7 +9,7 @@
  * @file loadMPC.cpp
  * @brief The implementation of the MPC decoder API
  * @author Rachel Mant <dx-mon@users.sourceforge.net>
- * @date 2010-2013
+ * @date 2010-2019
  */
 
 struct mpc_t::decoderContext_t final
@@ -64,7 +64,7 @@ namespace libAudio
 		* @internal
 		* This function applies a simple conversion algorithm to convert the input
 		* floating point MPC sample to a short for playback
-		* @param Sample The floating point sample to convert
+		* @param sample The floating point sample to convert
 		* @return The converted floating point sample
 		* @bug This function applies no noise shaping or dithering
 		*   So the output is sub-par to what it could be. FIXME!
@@ -83,15 +83,15 @@ namespace libAudio
 		* @internal
 		* \c read() is the internal read callback for MPC file decoding.
 		* This prevents nasty things from happening on Windows thanks to the run-time mess there.
-		* @param p_MPCFile Structure holding our own internal context pointer which
+		* @param reader Structure holding our own internal context pointer which
 		*   holds the file to seek through
-		* @param p_Buffer The buffer to read into
-		* @param size The number of bytes to read into the buffer
+		* @param buffer The buffer to read into
+		* @param bufferLen The number of bytes to read into the buffer
 		* @return The return result of \c fread()
 		*/
 		int32_t read(mpc_reader *reader, void *buffer, int bufferLen)
 		{
-			mpc_t *file = static_cast<mpc_t *>(reader->data);
+			const auto file = static_cast<mpc_t *>(reader->data);
 			return file->fd().read(buffer, bufferLen, nullptr);
 		}
 
@@ -99,14 +99,14 @@ namespace libAudio
 		* @internal
 		* \c seek() is the internal seek callback for MPC file decoding.
 		* This prevents nasty things from happening on Windows thanks to the run-time mess there.
-		* @param p_MPCFile Structure holding our own internal context pointer which
+		* @param reader Structure holding our own internal context pointer which
 		*   holds the file to seek through
 		* @param offset The offset through the file to which to seek to
 		* @return A truth value giving if the seek succeeded or not
 		*/
 		uint8_t seek(mpc_reader *reader, int offset)
 		{
-			mpc_t *file = static_cast<mpc_t *>(reader->data);
+			const auto file = static_cast<mpc_t *>(reader->data);
 			return file->fd().seek(offset, SEEK_SET) == offset;
 		}
 
@@ -114,13 +114,13 @@ namespace libAudio
 		* @internal
 		* \c tell() is the internal read possition callback for MPC file decoding.
 		* This prevents nasty things from happening on Windows thanks to the run-time mess there.
-		* @param p_MPCFile Structure holding our own internal context pointer which
+		* @param reader Structure holding our own internal context pointer which
 		*   holds the file to seek through
 		* @return An integer giving the read possition of the file in bytes
 		*/
 		int32_t tell(mpc_reader *reader)
 		{
-			mpc_t *file = static_cast<mpc_t *>(reader->data);
+			const auto file = static_cast<mpc_t *>(reader->data);
 			return file->fd().tell();
 		}
 
@@ -128,13 +128,13 @@ namespace libAudio
 		* @internal
 		* \c length() is the internal file length callback for MPC file decoding.
 		* This prevents nasty things from happening on Windows thanks to the run-time mess there.
-		* @param p_MPCFile Structure holding our own internal context pointer which
+		* @param reader Structure holding our own internal context pointer which
 		*   holds the file to seek through
 		* @return An integer giving the length of the file in bytes
 		*/
 		int32_t length(mpc_reader *reader)
 		{
-			mpc_t *file = static_cast<mpc_t *>(reader->data);
+			const auto file = static_cast<mpc_t *>(reader->data);
 			return file->fd().length();
 		}
 
@@ -143,15 +143,15 @@ namespace libAudio
 		* \c canSeek() is the internal callback for determining if an MPC file being
 		* decoded can be seeked on or not. \n This does two things: \n
 		* - It prevents nasty things from happening on Windows thanks to the run-time mess there
-		* - It uses \c fseek() as a no-operation to determine if we can seek or not.
+		* - It uses \c lseek() as a no-operation to determine if we can seek or not.
 		*
-		* @param p_MPCFile Structure holding our own internal context pointer which
+		* @param reader Structure holding our own internal context pointer which
 		*   holds the file to seek through
 		* @return A truth value giving if seeking can work or not
 		*/
 		uint8_t canSeek(mpc_reader *reader)
 		{
-			mpc_t *file = static_cast<mpc_t *>(reader->data);
+			const auto file = static_cast<mpc_t *>(reader->data);
 			return file->fd().tell() != -1;
 		}
 	}
@@ -188,7 +188,7 @@ mpc_t *mpc_t::openR(const char *const fileName) noexcept
  * This function opens the file given by \c FileName for reading and playback and returns a pointer
  * to the context of the opened file which must be used only by MPC_* functions
  * @param FileName The name of the file to open
- * @return A void pointer to the context of the opened file, or \c NULL if there was an error
+ * @return A void pointer to the context of the opened file, or \c nullptr if there was an error
  */
 void *MPC_OpenR(const char *FileName)
 {
@@ -207,9 +207,8 @@ void *MPC_OpenR(const char *FileName)
 /*!
  * This function gets the \c FileInfo structure for an opened file
  * @param p_MPCFile A pointer to a file opened with \c MPC_OpenR()
- * @return A \c FileInfo pointer containing various metadata about an opened file or \c NULL
+ * @return A \c FileInfo pointer containing various metadata about an opened file or \c nullptr
  * @warning This function must be called before using \c MPC_Play() or \c MPC_FillBuffer()
- * @bug \p p_MPCFile must not be NULL as no checking on the parameter is done. FIXME!
  */
 const fileInfo_t *MPC_GetFileInfo(void *p_MPCFile) { return audioFileInfo(p_MPCFile); }
 
@@ -222,7 +221,6 @@ const fileInfo_t *MPC_GetFileInfo(void *p_MPCFile) { return audioFileInfo(p_MPCF
  * @param countBufferLen An integer giving how long the output buffer is as a maximum fill-length
  * @return Either a negative value when an error condition is entered,
  * or the number of bytes written to the buffer
- * @bug \p p_MPCFile must not be NULL as no checking on the parameter is done. FIXME!
  */
 long MPC_FillBuffer(void *p_MPCFile, uint8_t *OutBuffer, int countBufferLen)
 	{ return audioFillBuffer(p_MPCFile, OutBuffer, countBufferLen); }
@@ -278,12 +276,11 @@ mpc_t::decoderContext_t::~decoderContext_t() noexcept
 
 /*!
  * Closes an opened audio file
- * @param p_MPCFile A pointer to a file opened with \c MPC_OpenR(), or \c NULL for a no-operation
+ * @param p_MPCFile A pointer to a file opened with \c MPC_OpenR(), or \c nullptr for a no-operation
  * @return an integer indicating success or failure with the same values as \c fclose()
  * @warning Do not use the pointer given by \p p_MPCFile after using
- * this function - please either set it to \c NULL or be extra carefull
+ * this function - please either set it to \c nullptr or be extra carefull
  * to destroy it via scope
- * @bug \p p_MPCFile must not be NULL as no checking on the parameter is done. FIXME!
  */
 int MPC_CloseFileR(void *p_MPCFile) { return audioCloseFile(p_MPCFile); }
 
@@ -293,10 +290,6 @@ int MPC_CloseFileR(void *p_MPCFile) { return audioCloseFile(p_MPCFile); }
  * @warning If \c ExternalPlayback was a non-zero value for
  * the call to \c MPC_OpenR() used to open the file at \p p_MPCFile,
  * this function will do nothing.
- * @bug \p p_MPCFile must not be NULL as no checking on the parameter is done. FIXME!
- *
- * @bug Futher to the \p p_MPCFile check bug on this function, if this function is
- *   called as a no-op as given by the warning, then it will also cause the same problem. FIXME!
  */
 void MPC_Play(void *p_MPCFile) { audioPlay(p_MPCFile); }
 void MPC_Pause(void *p_MPCFile) { audioPause(p_MPCFile); }
