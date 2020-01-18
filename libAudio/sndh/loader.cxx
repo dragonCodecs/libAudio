b9f09b3c6ce30f14a18a28f90732146d04ae0f0c
dx-mon <dx-mon@users.sourceforge.net>
2020-01-18 13:52:55 +0000

    Implemented the SNDH tag type constants

--- libAudio/sndh/loader.cxx
+++ libAudio/sndh/loader.cxx
@@ -1,7 +1,30 @@
 #include <cstring>
+#include <type_traits>
 #include "loader.hxx"
+#include "../conversions.hxx"
 
-sndhLoader_t::sndhLoader_t(const fd_t &file) : _entryPoints{}
+using namespace libAudio::conversions;
+
+constexpr std::array<char, 4> typeHeader{'S', 'N', 'D', 'H'};
+constexpr std::array<char, 4> typeTitle{'T', 'I', 'T', 'L'};
+constexpr std::array<char, 4> typeComposer{'C', 'O', 'M', 'M'};
+constexpr std::array<char, 4> typeRipper{'R', 'I', 'P', 'P'};
+constexpr std::array<char, 4> typeConverter{'C', 'O', 'N', 'V'};
+constexpr std::array<char, 2> typeTuneCount{'#', '#'};
+constexpr std::array<char, 2> typeTimerA{'T', 'A'};
+constexpr std::array<char, 2> typeTimerB{'T', 'B'};
+constexpr std::array<char, 2> typeTimerC{'T', 'C'};
+constexpr std::array<char, 2> typeTimerD{'T', 'D'};
+constexpr std::array<char, 2> typeTimerVBL{'!', 'V'};
+constexpr std::array<char, 4> typeYear{'Y', 'E', 'A', 'R'};
+constexpr std::array<char, 4> typeTime{'T', 'I', 'M', 'E'};
+constexpr std::array<char, 4> typeEnd{'H', 'D', 'N', 'S'};
+
+template<typename T, size_t sizeA, size_t sizeB, typename = typename std::enable_if<sizeA != sizeB>::type>
+	bool operator ==(const std::array<T, sizeA> &a, const std::array<T, sizeB> &b) noexcept
+	{ return memcmp(a.data(), b.data(), std::min(sizeA, sizeB)) == 0; }
+
+sndhLoader_t::sndhLoader_t(const fd_t &file) : _entryPoints{}, _metadata{}
 {
 	std::array<char, 4> sndhSig{};
 	if (!file.readBE(_entryPoints.init) ||
