7f5e9daeb05fe70230960db33e71d972dce808c3
dx-mon <dx-mon@users.sourceforge.net>
2020-02-09 14:50:48 +0000

    Reworked and fixed up fixedVector_t reducing clang-tidy puke for it and fixing some known bugs along the way

--- libAudio/fixedVector.hxx
+++ libAudio/fixedVector.hxx
@@ -20,35 +20,33 @@ public:
 	using pointer = T *;
 	using reference = T &;
 
-	constexpr boundedIterator_t(T *const ptr, const size_t _max) noexcept : boundedIterator_t{ptr, 0, _max} { }
-	boundedIterator_t(const boundedIterator_t &iter) noexcept : boundedIterator_t{iter.data, iter.index, iter.max} { }
-	boundedIterator_t(boundedIterator_t &&iter) noexcept : boundedIterator_t{iter.data, iter.index, iter.max} { }
+	constexpr boundedIterator_t(T *const ptr, const size_t _max) noexcept :
+		data{ptr}, index{0}, max{_max} { }
+	boundedIterator_t(const boundedIterator_t &) noexcept = default;
+	boundedIterator_t(boundedIterator_t &&) noexcept = default;
+	~boundedIterator_t() noexcept = default;
+	boundedIterator_t &operator =(const boundedIterator_t &) noexcept = default;
+	boundedIterator_t &operator =(boundedIterator_t &&) noexcept = default;
 
 	reference operator *() const noexcept { return data[index]; }
 	pointer operator ->() const noexcept { return data + index; }
 	boundedIterator_t &operator ++() noexcept { if (index < max) ++index; return *this; }
-	boundedIterator_t &operator ++(int) noexcept
-		{ if (index < max) return boundedIterator_t(data, index++, max); return *this; }
+	const boundedIterator_t operator ++(int) noexcept
+		{ if (index < max) return {data, index++, max}; return *this; }
 	boundedIterator_t &operator --() noexcept { if (index > 0) --index; return *this; }
-	boundedIterator_t &operator --(int) noexcept
-		{ if (index > 0) return boundedIterator_t(data, index--, max); return *this; }
+	const boundedIterator_t operator --(int) noexcept
+		{ if (index > 0) return {data, index--, max}; return *this; }
 	reference operator [](const size_t n) const noexcept { return *(*this + n); }
-	// I think the > max case must also be addressed..
-	boundedIterator_t &operator +=(const size_t n) noexcept { if ((index + n) <= max) index += n; return *this; }
-	boundedIterator_t &operator +(const size_t n) const noexcept { return boundedIterator_t{*this} += n; }
-	// The supper complicated looking condition here checkes that the subtraction won't underflow
-	// and go super-large, in a way where we don't have to care what size_t really means
-	// I think the < 0 case must also be addressed..
+	boundedIterator_t &operator +=(const size_t n) noexcept
+		{ (index + n) < max && (index + n) >= index ? index += n : index = max; return *this; }
+	boundedIterator_t operator +(const size_t n) const noexcept { return boundedIterator_t{*this} += n; }
 	boundedIterator_t &operator -=(const size_t n) noexcept
-		{ if (!((index - n) & (1 << ((sizeof(size_t) * 8) - 1)))) index -= n; return *this; }
-	boundedIterator_t &operator -(const size_t n) const noexcept { return boundedIterator_t{*this} -= n; }
+		{ n < index ? index -= n : index = 0; return *this; }
+	boundedIterator_t operator -(const size_t n) const noexcept { return boundedIterator_t{*this} -= n; }
 
 	bool operator ==(const boundedIterator_t &b) const noexcept
 		{ return data == b.data && index == b.index && max == b.max; }
 	bool operator !=(const boundedIterator_t &b) const noexcept { return !(*this == b); }
-
-	boundedIterator_t operator =(const boundedIterator_t &) noexcept = delete;
-	boundedIterator_t operator =(boundedIterator_t &&) noexcept = delete;
 };
 
 struct vectorStateException_t final : public std::exception
@@ -72,37 +70,40 @@ public:
 	using iterator = boundedIterator_t<T>;
 	using const_iterator = boundedIterator_t<const T>;
 
-	constexpr fixedVector_t() noexcept : _data(), _length(0) { }
+	constexpr fixedVector_t() noexcept : _data{}, _length{0} { }
 	fixedVector_t(const size_t length) noexcept : _data(length ? new (std::nothrow) T[length]() : nullptr),
 		_length(length) { }
-	fixedVector_t(fixedVector_t &&vec) noexcept : fixedVector_t() { swap(vec); }
-	//~fixedVector_t() noexcept { }
+	fixedVector_t(fixedVector_t &&vec) noexcept : fixedVector_t{} { swap(vec); }
+	~fixedVector_t() noexcept = default;
 	fixedVector_t &operator =(fixedVector_t &&vec) noexcept { swap(vec); return *this; }
 
 	size_t length() const noexcept { return _length; }
 	size_t size() const noexcept { return _length; }
+	size_t count() const noexcept { return _length; }
 	bool valid() const noexcept { return bool(_data); }
+	T *data() const noexcept { return _data.get(); }
+	explicit operator bool() const noexcept { return valid(); }
 
 	reference operator [](const size_t index)
 	{
 		if (!valid())
-			throw vectorStateException_t();
+			throw vectorStateException_t{};
 		else if (index < _length)
 			return _data[index];
-		throw std::out_of_range("Index into fixedVector_t out of bounds");
+		throw std::out_of_range{"Index into fixedVector_t out of bounds"};
 	}
 
 	const_reference operator [](const size_t index) const
 	{
 		if (!valid())
-			throw vectorStateException_t();
+			throw vectorStateException_t{};
 		else if (index < _length)
 			return _data[index];
-		throw std::out_of_range("Index into fixedVector_t out of bounds");
+		throw std::out_of_range{"Index into fixedVector_t out of bounds"};
 	}
 
-	iterator begin() noexcept { return iterator{_data.get(), _length}; }
-	const_iterator begin() const noexcept { return const_iterator{_data.get(), _length}; }
+	iterator begin() noexcept { return {_data.get(), _length}; }
+	const_iterator begin() const noexcept { return {_data.get(), _length}; }
 	iterator end() noexcept { return begin() += _length; }
 	const_iterator end() const noexcept { return begin() += _length; }
 
@@ -115,5 +116,8 @@ public:
 	fixedVector_t(const fixedVector_t &) = delete;
 	fixedVector_t &operator =(const fixedVector_t &) = delete;
 };
+
+template<typename T> inline void swap(fixedVector_t<T> &a, fixedVector_t<T> &b) noexcept
+	{ a.swap(b); }
 
 #endif /*FIXED_VECTOR__HXX*/
