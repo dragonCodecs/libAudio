a27046aa6f5db26fa081f24f4ab4f185b399a3d9
dragonmux <git@dragonmux.network>
2021-10-17 03:00:16 +0100

    Added OBS integration support to the player

--- player/player.cxx
+++ player/player.cxx
@@ -1,12 +1,34 @@
-#include <stdint.h>
+#include <cstdint>
 #include <libAudio.h>
+#include <string>
+#include <substrate/fd>
 // XXX: This header actually needs installing and the current header mess figured out + fixed.
 #include "../libAudio/console.hxx"
 
 using libAudio::console::asTime_t;
 
+#if STREAMING
+inline std::string operator""_s(const char *const str, const size_t len)
+	{ return std::string{str, len}; }
+#endif
+
 #ifdef _WINDOWS
 void invalidHandler(const wchar_t *, const wchar_t *, const wchar_t *, const uint32_t, const uintptr_t) { }
+#endif
+
+#if STREAMING
+static void writeNowPlaying(const char *const file, const char *const title)
+{
+	using namespace substrate;
+	fd_t fd{"/tmp/musicStreamer.playing", O_RDWR | O_NOCTTY | O_CREAT | O_TRUNC, normalMode};
+	if (!fd.valid())
+		return;
+	fd.write("Now Playing: "_s);
+	fd.write(file, strlen(file));
+	fd.write(" ("_s);
+	fd.write(title, strlen(title));
+	fd.write(")    "_s);
+}
 #endif
 
 int main(int argc, char **argv)
@@ -35,6 +57,9 @@ int main(int argc, char **argv)
 		console.info("File '", argv[i], "', TotalTime: ", asTime_t{info->totalTime}, ", Sample Rate: ", info->bitRate,
 			"Hz, Title: ", info->title, ", Artist: ", info->artist, ", Album: ", info->album, ", Channels: ",
 			info->channels);
+#if STREAMING
+		writeNowPlaying(argv[i], info->title.get());
+#endif
 
 		audioPlay(audioFile);
 		audioCloseFile(audioFile);
