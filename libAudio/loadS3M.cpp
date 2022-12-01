12029a65abadd1f3323a964e8a109e206c330af8
dragonmux <git@dragonmux.network>
2022-12-01 20:26:27 +0000

    loadS3M: Cleaned up and converted the magic number code for isS3M() to std::array<>

--- libAudio/loadS3M.cpp
+++ libAudio/loadS3M.cpp
@@ -3,6 +3,12 @@
 #include "genericModule/genericModule.h"
 #include "console.hxx"
 
+namespace libAudio::s3m
+{
+	constexpr static char s3mMagic1{'\x1A'};
+	constexpr static std::array<char, 4> s3mMagic2{{'S', 'C', 'R', 'M'}};
+}
+
 modS3M_t::modS3M_t(fd_t &&fd) noexcept : moduleFile_t{audioType_t::moduleS3M, std::move(fd)} { }
 
 modS3M_t *modS3M_t::openR(const char *const fileName) noexcept
@@ -40,15 +46,16 @@ bool modS3M_t::isS3M(const int32_t fd) noexcept
 {
 	constexpr const uint32_t seekOffset1 = 28;
 	constexpr const uint32_t seekOffset2 = seekOffset1 + 16;
-	char S3MMagic1, S3MMagic2[4];
+	char s3mMagic1;
+	std::array<char, 4> s3mMagic2;
 	if (fd == -1 ||
 		lseek(fd, seekOffset1, SEEK_SET) != seekOffset1 ||
-		read(fd, &S3MMagic1, 1) != 1 ||
+		read(fd, &s3mMagic1, 1) != 1 ||
 		lseek(fd, seekOffset2, SEEK_SET) != seekOffset2 ||
-		read(fd, S3MMagic2, 4) != 4 ||
+		read(fd, s3mMagic2.data(), s3mMagic2.size()) != s3mMagic2.size() ||
 		lseek(fd, 0, SEEK_SET) != 0 ||
-		S3MMagic1 != 0x1A ||
-		memcmp(S3MMagic2, "SCRM", 4) != 0)
+		s3mMagic1 != libAudio::s3m::s3mMagic1 ||
+		s3mMagic2 != libAudio::s3m::s3mMagic2)
 		return false;
 	else
 		return true;
