93b8cb582ed34b33cf7f7d85f4831cfb8e87a695
dragonmux <git@dragonmux.network>
2023-03-23 01:49:44 +0000

    Expanded on the fileInfo_t test suite

--- test/libAudio/testFileInfo.cxx
+++ test/libAudio/testFileInfo.cxx
@@ -12,6 +12,16 @@ private:
 		assertEqual(fileInfo.bitsPerSample(), 0U);
 		assertEqual(fileInfo.bitRate(), 0U);
 		assertEqual(fileInfo.channels(), 0U);
+
+		fileInfo.totalTime(UINT64_MAX);
+		fileInfo.bitsPerSample(UINT32_MAX);
+		fileInfo.bitRate(UINT32_MAX);
+		fileInfo.channels(UINT8_MAX);
+
+		assertEqual(fileInfo.totalTime(), UINT64_MAX);
+		assertEqual(fileInfo.bitsPerSample(), UINT32_MAX);
+		assertEqual(fileInfo.bitRate(), UINT32_MAX);
+		assertEqual(fileInfo.channels(), UINT8_MAX);
 	}
 
 	void testFileInfoC()
@@ -20,6 +30,22 @@ private:
 		assertEqual(audioFileBitsPerSample(nullptr), 0U);
 		assertEqual(audioFileBitRate(nullptr), 0U);
 		assertEqual(audioFileChannels(nullptr), 0U);
+
+		fileInfo_t fileInfo{};
+		assertEqual(audioFileTotalTime(&fileInfo), 0U);
+		assertEqual(audioFileBitsPerSample(&fileInfo), 0U);
+		assertEqual(audioFileBitRate(&fileInfo), 0U);
+		assertEqual(audioFileChannels(&fileInfo), 0U);
+
+		fileInfo.totalTime(UINT64_MAX);
+		fileInfo.bitsPerSample(UINT32_MAX);
+		fileInfo.bitRate(UINT32_MAX);
+		fileInfo.channels(UINT8_MAX);
+
+		assertEqual(audioFileTotalTime(&fileInfo), UINT64_MAX);
+		assertEqual(audioFileBitsPerSample(&fileInfo), UINT32_MAX);
+		assertEqual(audioFileBitRate(&fileInfo), UINT32_MAX);
+		assertEqual(audioFileChannels(&fileInfo), UINT8_MAX);
 	}
 
 public:
