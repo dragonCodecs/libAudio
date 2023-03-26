1c9dadca7dea8c4bcd4578105697e7f94fb1f4b2
dragonmux <git@dragonmux.network>
2023-03-26 02:53:12 +0100

    Moved the implementation of fileInfo_t's functions into the implementation file so we can change them without further ABI breaks

--- libAudio/fileInfo.cxx
+++ libAudio/fileInfo.cxx
@@ -3,7 +3,65 @@
 #include "libAudio.h"
 #include "fileInfo.hxx"
 
-[[nodiscard]] const char *fileInfo_t::otherComment(size_t index) const noexcept
+fileInfo_t::fileInfo_t() noexcept = default;
+fileInfo_t::fileInfo_t(fileInfo_t &&) noexcept = default;
+fileInfo_t &fileInfo_t::operator =(fileInfo_t &&) noexcept = default;
+fileInfo_t::~fileInfo_t() noexcept = default;
+
+void fileInfo_t::operator =(const fileInfo_t &info) noexcept
+{
+	if (this == &info)
+		return;
+	_totalTime = info._totalTime;
+	_bitsPerSample = info._bitsPerSample;
+	_bitRate = info._bitRate;
+	_channels = info._channels;
+}
+
+uint64_t fileInfo_t::totalTime() const noexcept
+	{ return _totalTime; }
+void fileInfo_t::totalTime(const uint64_t totalTime) noexcept
+	{ _totalTime = totalTime; }
+uint32_t fileInfo_t::bitsPerSample() const noexcept
+	{ return _bitsPerSample; }
+void fileInfo_t::bitsPerSample(const uint32_t bitsPerSample) noexcept
+	{ _bitsPerSample = bitsPerSample; }
+uint32_t fileInfo_t::bitRate() const noexcept
+	{ return _bitRate; }
+void fileInfo_t::bitRate(const uint32_t bitRate) noexcept
+	{ _bitRate = bitRate; }
+uint8_t fileInfo_t::channels() const noexcept
+	{ return _channels; }
+void fileInfo_t::channels(const uint8_t channels) noexcept
+	{ _channels = channels; }
+
+const char *fileInfo_t::title() const noexcept
+	{ return _title.get(); }
+std::unique_ptr<char []> &fileInfo_t::titlePtr() noexcept
+	{ return _title; }
+void fileInfo_t::title(std::unique_ptr<char []> &&title) noexcept
+	{ _title = std::move(title); }
+const char *fileInfo_t::artist() const noexcept
+	{ return _artist.get(); }
+std::unique_ptr<char []> &fileInfo_t::artistPtr() noexcept
+	{ return _artist; }
+void fileInfo_t::artist(std::unique_ptr<char []> &&artist) noexcept
+	{ _artist = std::move(artist); }
+const char *fileInfo_t::album() const noexcept
+	{ return _album.get(); }
+std::unique_ptr<char []> &fileInfo_t::albumPtr() noexcept
+	{ return _album; }
+void fileInfo_t::album(std::unique_ptr<char []> &&album) noexcept
+	{ _album = std::move(album); }
+
+const std::vector<std::unique_ptr<char []>> &fileInfo_t::other() const noexcept
+	{ return _other; }
+size_t fileInfo_t::otherCommentsCount() const noexcept
+	{ return _other.size(); }
+void fileInfo_t::addOtherComment(std::unique_ptr<char []> &&comment) noexcept
+	{ _other.emplace_back(std::move(comment)); }
+
+const char *fileInfo_t::otherComment(size_t index) const noexcept
 {
 	if (index >= _other.size())
 		return nullptr;
--- libAudio/fileInfo.hxx
+++ libAudio/fileInfo.hxx
@@ -6,8 +6,9 @@
 #include <cstdint>
 #include <memory>
 #include <vector>
+#include "libAudio.h"
 
-struct fileInfo_t final
+struct libAUDIO_CLS_API fileInfo_t final
 {
 private:
 	uint64_t _totalTime{0};
@@ -21,47 +22,38 @@ private:
 	std::vector<std::unique_ptr<char []>> _other{};
 
 public:
-	fileInfo_t() noexcept = default;
-	fileInfo_t(fileInfo_t &&) = default;
-	fileInfo_t &operator =(fileInfo_t &&) = default;
-	~fileInfo_t() noexcept = default;
+	fileInfo_t() noexcept;
+	fileInfo_t(fileInfo_t &&) noexcept;
+	fileInfo_t &operator =(fileInfo_t &&) noexcept;
+	~fileInfo_t() noexcept;
 
-	void operator =(const fileInfo_t &info) noexcept
-	{
-		if (this == &info)
-			return;
-		_totalTime = info._totalTime;
-		_bitsPerSample = info._bitsPerSample;
-		_bitRate = info._bitRate;
-		_channels = info._channels;
-	}
+	void operator =(const fileInfo_t &info) noexcept;
 
 	fileInfo_t(const fileInfo_t &) = delete;
 
-	[[nodiscard]] uint64_t totalTime() const noexcept { return _totalTime; }
-	void totalTime(const uint64_t totalTime) noexcept { _totalTime = totalTime; }
-	[[nodiscard]] uint32_t bitsPerSample() const noexcept { return _bitsPerSample; }
-	void bitsPerSample(const uint32_t bitsPerSample) noexcept { _bitsPerSample = bitsPerSample; }
-	[[nodiscard]] uint32_t bitRate() const noexcept { return _bitRate; }
-	void bitRate(const uint32_t bitRate) noexcept { _bitRate = bitRate; }
-	[[nodiscard]] uint8_t channels() const noexcept { return _channels; }
-	void channels(const uint8_t channels) noexcept { _channels = channels; }
+	[[nodiscard]] uint64_t totalTime() const noexcept;
+	void totalTime(const uint64_t totalTime) noexcept;
+	[[nodiscard]] uint32_t bitsPerSample() const noexcept;
+	void bitsPerSample(const uint32_t bitsPerSample) noexcept;
+	[[nodiscard]] uint32_t bitRate() const noexcept;
+	void bitRate(const uint32_t bitRate) noexcept;
+	[[nodiscard]] uint8_t channels() const noexcept;
+	void channels(const uint8_t channels) noexcept;
 
-	[[nodiscard]] const char *title() const noexcept { return _title.get(); }
-	[[nodiscard]] std::unique_ptr<char []> &titlePtr() noexcept { return _title; }
-	void title(std::unique_ptr<char []> &&title) noexcept { _title = std::move(title); }
-	[[nodiscard]] const char *artist() const noexcept { return _artist.get(); }
-	[[nodiscard]] std::unique_ptr<char []> &artistPtr() noexcept { return _artist; }
-	void artist(std::unique_ptr<char []> &&artist) noexcept { _artist = std::move(artist); }
-	[[nodiscard]] const char *album() const noexcept { return _album.get(); }
-	[[nodiscard]] std::unique_ptr<char []> &albumPtr() noexcept { return _album; }
-	void album(std::unique_ptr<char []> &&album) noexcept { _album = std::move(album); }
+	[[nodiscard]] const char *title() const noexcept;
+	[[nodiscard]] std::unique_ptr<char []> &titlePtr() noexcept;
+	void title(std::unique_ptr<char []> &&title) noexcept;
+	[[nodiscard]] const char *artist() const noexcept;
+	[[nodiscard]] std::unique_ptr<char []> &artistPtr() noexcept;
+	void artist(std::unique_ptr<char []> &&artist) noexcept;
+	[[nodiscard]] const char *album() const noexcept;
+	[[nodiscard]] std::unique_ptr<char []> &albumPtr() noexcept;
+	void album(std::unique_ptr<char []> &&album) noexcept;
 
-	[[nodiscard]] const std::vector<std::unique_ptr<char []>> &other() const noexcept { return _other; }
-	[[nodiscard]] size_t otherCommentsCount() const noexcept { return _other.size(); }
+	[[nodiscard]] const std::vector<std::unique_ptr<char []>> &other() const noexcept;
+	[[nodiscard]] size_t otherCommentsCount() const noexcept;
 	[[nodiscard]] const char *otherComment(size_t index) const noexcept;
-	void addOtherComment(std::unique_ptr<char []> &&comment) noexcept
-		{ _other.emplace_back(std::move(comment)); }
+	void addOtherComment(std::unique_ptr<char []> &&comment) noexcept;
 };
 
 #endif /*FILE_INFO_HXX*/
