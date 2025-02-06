02dde264711d371380127f26ddc2018c915b02c6
dragonmux <git@dragonmux.network>
2025-02-06 02:10:14 +0000

    emulator/cpu/m68k: Added restrictions on the decode for MOVE from CCR to follow the effective addressing table for the instruction

--- libAudio/emulator/cpu/m68k.cxx
+++ libAudio/emulator/cpu/m68k.cxx
@@ -1223,6 +1223,13 @@ decodedOperation_t motorola68000_t::decodeInstruction(const uint16_t insn) const
 				eaMode,
 			};
 		case 0x42c0U:
+			// MOVE from CCR is not allowed with address registers
+			if (eaMode == 1U)
+				break;
+			// MOVE from CCR is not allowed with `#<data>` mode or PC-rel data register usage,
+			// only u16 and u32 indirect mode 7
+			if (eaMode == 7U && !(eaReg == 0U || eaReg == 1U))
+				break;
 			return
 			{
 				instruction_t::move,
