c1158016e1e50fb7c46dab98b9b0ba3562fcd675
dragonmux <git@dragonmux.network>
2025-02-20 18:38:41 -0800

    emulator/sound/ym2149: Implemented register selection for the control I/O interface

--- libAudio/emulator/sound/ym2149.cxx
+++ libAudio/emulator/sound/ym2149.cxx
@@ -14,4 +14,18 @@ void ym2149_t::readAddress(const uint32_t address, substrate::span<uint8_t> data
 
 void ym2149_t::writeAddress(const uint32_t address, const substrate::span<uint8_t> &data) noexcept
 {
+	// Only admit 8- and 16-bit writes
+	if (data.size_bytes() > 2U)
+		return;
+	// The address to write determines if we're making a register selection or writing a register
+	switch (address)
+	{
+		case 0U: // Register selection
+			// Only the bottom 4 bits of the selection are valid/used
+			selectedRegister = data[0U] & 0x0fU;
+			break;
+		case 2U: // Register write
+			//
+			break;
+	}
 }
