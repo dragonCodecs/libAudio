4bd36b9f0fbb728e8dadbf71fc1f901531830049
dragonmux <git@dragonmux.network>
2025-02-25 21:51:35 -0800

    emulator/sound/steDAC: Implemented the read half of the register interface for Microwire

--- libAudio/emulator/sound/steDAC.cxx
+++ libAudio/emulator/sound/steDAC.cxx
@@ -8,6 +8,26 @@ steDAC_t::steDAC_t(const uint32_t clockFrequency) noexcept : clockedPeripheral_t
 
 void steDAC_t::readAddress(const uint32_t address, substrate::span<uint8_t> data) const noexcept
 {
+	// Extract how wide an access this is
+	const auto accessWidth{data.size_bytes()};
+	// Microwire registers are only accessible as u16's
+	if (accessWidth == 2U)
+	{
+		// u16 accesses must be to even addresses
+		if ((address & 1U) != 0U)
+			return;
+
+		// Handle microwire address accesses
+		switch (address)
+		{
+			case 0x22U:
+				writeBE(microwireData, data);
+				return;
+			case 0x24U:
+				writeBE(microwireCycle(), data);
+				return;
+		}
+	}
 }
 
 void steDAC_t::writeAddress(const uint32_t address, const substrate::span<uint8_t> &data) noexcept
@@ -17,4 +37,16 @@ void steDAC_t::writeAddress(const uint32_t address, const substrate::span<uint8_
 bool steDAC_t::clockCycle() noexcept
 {
 	return true;
+}
+
+uint16_t steDAC_t::microwireCycle() const noexcept
+{
+	// If there are still cycles to do
+	if (microwireCycles)
+	{
+		--microwireCycles;
+		// Rotate the mask one more bit left
+		microwireMask = (microwireMask << 1U) | (microwireMask >> 15U);
+	}
+	return microwireMask;
 }
--- libAudio/emulator/sound/steDAC.hxx
+++ libAudio/emulator/sound/steDAC.hxx
@@ -12,6 +12,14 @@ private:
 	void readAddress(uint32_t address, substrate::span<uint8_t> data) const noexcept override;
 	void writeAddress(uint32_t address, const substrate::span<uint8_t> &data) noexcept override;
 
+	// NB: only reason for these to be marked mutable is so microwireCycle() can be const
+	// for use in the readAddress call - not ideal, but not completely terrible
+	uint16_t microwireData{0U};
+	mutable uint16_t microwireMask{0U};
+	mutable uint8_t microwireCycles{0U};
+
+	[[nodiscard]] uint16_t microwireCycle() const noexcept;
+
 public:
 	steDAC_t(uint32_t clockFrequency) noexcept;
 	steDAC_t(const steDAC_t &) = default;
