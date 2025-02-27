0a3549cadcf6eedec38e8b3a7663bf411346c8aa
dragonmux <git@dragonmux.network>
2025-02-26 17:34:52 -0800

    emulator/gemdosAlloc: Implemented the allocation half of the puzzle

--- libAudio/emulator/gemdosAlloc.cxx
+++ libAudio/emulator/gemdosAlloc.cxx
@@ -3,8 +3,22 @@
 #include <cstdint>
 #include "gemdosAlloc.hxx"
 
+// Define the alignment requirements on allocations
+constexpr static size_t mallocAlignment{8U};
+constexpr static size_t chunkAlignment{4U};
+// Figure out how much padding is needed for an allocation
+constexpr static size_t mallocPadding{std::max(mallocAlignment, chunkAlignment) - chunkAlignment};
+// Allow no smaller than a pointer wide allocation on the target platform
+constexpr static size_t mallocMinSize{4U};
+// The minimal chunk size is the padding + the minimum allocation size
+constexpr static size_t mallocMinChunkSize{mallocPadding + mallocMinSize};
+
+// NB: alignment must be a power of two coming into this function(!)
+constexpr static inline size_t align(const size_t size, const size_t alignment) noexcept
+	{ return (size + alignment - 1U) & ~(alignment - 1U); }
+
 gemdosAllocator_t::gemdosAllocator_t(const uint32_t base, const uint32_t size) noexcept :
-	heapStart{base}, heapEnd{base + size}, heapCurrent{base} { }
+	heapBegin{base}, heapEnd{base + size}, heapCurrent{base} { }
 
 uint32_t gemdosAllocator_t::changeHeapSize(const int32_t amount) noexcept
 {
@@ -17,4 +31,97 @@ uint32_t gemdosAllocator_t::changeHeapSize(const int32_t amount) noexcept
 	const auto result{heapCurrent};
 	heapCurrent += amount;
 	return result;
+}
+
+std::optional<uint32_t> gemdosAllocator_t::alloc(const size_t size) noexcept
+{
+	// Allocations of 0 bytes are not allowed
+	if (size == 0U)
+		return std::nullopt;
+
+	const auto allocSize
+	{
+		[&]() -> size_t
+		{
+			// Figure out allocation padding and size
+			const auto alignedSize{align(size, chunkAlignment) + mallocPadding};
+			return std::max(alignedSize, mallocMinChunkSize);
+		}()
+	};
+
+	// Check if the allocation is just too big, or if the computed size is less than requested)
+	if (allocSize > heapEnd - heapBegin || allocSize < size)
+		return std::nullopt;
+
+	// Set up to store where we can allocate from
+	allocChunk_t chunk{};
+
+	// Now step through the free list by iterator
+	for (auto entry{freeList.begin()}; entry != freeList.end(); ++entry)
+	{
+		// Compute how much memory would be remaining if this free chunk was used
+		const auto remainder{static_cast<int32_t>(entry->size - allocSize)};
+		// If it would fit, then
+		if (remainder >= 0)
+		{
+			// Check if the remainder is enough for another chunk
+			if (static_cast<size_t>(remainder) >= mallocMinChunkSize)
+			{
+				// Split the node, and extract the necessary memory for the allocation
+				chunk.base = entry->base;
+				chunk.size = allocSize;
+				entry->base += allocSize;
+				entry->size -= allocSize;
+			}
+			// If we found chunk that's exactly or barely bigger than the right size,
+			// extract it from the free list entirely
+			else
+			{
+				chunk = *entry;
+				freeList.erase(entry);
+			}
+			// Happy case, we're done now!
+			break;
+		}
+	}
+
+	// Check to see if we managed to liberate a chunk from the free list, if not
+	// then ask for some more memory from the heap
+	if (chunk.size == 0U)
+	{
+		// Get some memory
+		chunk.base = changeHeapSize(allocSize);
+		// If that failed
+		if (chunk.base == UINT32_MAX)
+		{
+			// If the free list is empty, we're done
+			if (freeList.size() == 0U)
+				return std::nullopt;
+			// Check to see if the last item in the free list could be grown
+			// if it's at the end of the heap
+			const auto entry{freeList.back()};
+			if (entry.base + entry.size == heapCurrent)
+			{
+				// Ask for the difference and extract the node if that succeeds
+				if (changeHeapSize(allocSize - entry.size) == UINT32_MAX)
+					return std::nullopt;
+				chunk.base = entry.base;
+				chunk.size = allocSize;
+				freeList.pop_back();
+			}
+			// Otherwise, we couldn't find any more free memory
+			else
+				return std::nullopt;
+		}
+		// Otherwise, hppy case - set the allocation size
+		else
+			chunk.size = allocSize;
+	}
+	// Happy days, we got some memory - return it!
+	if (chunk.size != 0U)
+	{
+		allocList.push_back(chunk);
+		return chunk.base;
+	}
+	return std::nullopt;
 }
--- libAudio/emulator/gemdosAlloc.hxx
+++ libAudio/emulator/gemdosAlloc.hxx
@@ -4,17 +4,30 @@
 #define EMULATOR_GEMDOS_ALLOC_HXX
 
 #include <cstdint>
+#include <vector>
+#include <optional>
+
+struct allocChunk_t final
+{
+	uint32_t base{0U};
+	uint32_t size{0U};
+};
 
 struct gemdosAllocator_t final
 {
-	uint32_t heapStart;
+	uint32_t heapBegin;
 	uint32_t heapEnd;
 	uint32_t heapCurrent;
+
+	std::vector<allocChunk_t> freeList{};
+	std::vector<allocChunk_t> allocList{};
 
 	[[nodiscard]] uint32_t changeHeapSize(int32_t amount) noexcept;
 
 public:
 	gemdosAllocator_t(uint32_t base, uint32_t size) noexcept;
+
+	[[nodiscard]] std::optional<uint32_t> alloc(size_t size) noexcept;
 };
 
 #endif /*EMULATOR_GEMDOS_ALLOC_HXX*/
