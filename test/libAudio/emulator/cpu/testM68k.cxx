a74271c30adafe09e74ff3a7c32029a22b8ee21b
dragonmux <git@dragonmux.network>
2025-03-02 10:24:02 +0000

    test/emulator/cpu/testM68k: Created a test that the branch instruction handling works right

--- test/libAudio/emulator/cpu/testM68k.cxx
+++ test/libAudio/emulator/cpu/testM68k.cxx
@@ -21,6 +21,52 @@ private:
 			assertTrue(cpu.decodeInstruction(insn) == decodedOperation);
 	}
 
+	void runStep(const bool expectingTrap = false)
+	{
+		const auto result{cpu.step()};
+		assertTrue(result.validInsn);
+		if (expectingTrap)
+			assertTrue(result.trap);
+		else
+			assertFalse(result.trap);
+		// TODO: Check the cycles taken matches expectations
+	}
+
+	void testBranch()
+	{
+		// Set up 3 branches in each of the 3 instruction encooding forms, checking they jump properly to each other
+		// Start with the 8-bit immediate form, then use the 16-bit immediate form, and finally the 32-bit immediate
+		// form (6 bytes to encode)
+		writeAddress(0x000100U, uint16_t{0x6050U}); // Jump to +0x50 from end of instruction
+		writeAddress(0x000152U, uint16_t{0x6000U});
+		writeAddress(0x000154U, uint16_t{0xfefcU}); // Jump to -0x104 from end of instruction
+		writeAddress(0x000050U, uint16_t{0x60ffU});
+		writeAddress(0x000052U, uint16_t{0x0001U}); // Jump to +0x100ae from end of instruction
+		writeAddress(0x000054U, uint16_t{0x00aeU});
+		writeAddress(0x010100U, uint16_t{0x4e75U}); // RTS to end the test
+		// Set the CPU to execute this sequence
+		cpu.executeFrom(0x00000100U, 0x00800000U);
+		// Validate starting conditions
+		assertEqual(cpu.readProgramCounter(), 0x00000100U);
+		assertEqual(cpu.readAddrRegister(7U), 0x007ffffcU);
+		// Step the first instruction and validate
+		runStep();
+		assertEqual(cpu.readProgramCounter(), 0x00000152U);
+		assertEqual(cpu.readAddrRegister(7U), 0x007ffffcU);
+		// Step the second instruction and validate
+		runStep();
+		assertEqual(cpu.readProgramCounter(), 0x00000050U);
+		assertEqual(cpu.readAddrRegister(7U), 0x007ffffcU);
+		// Step the third instruction and validate
+		runStep();
+		assertEqual(cpu.readProgramCounter(), 0x00010100U);
+		assertEqual(cpu.readAddrRegister(7U), 0x007ffffcU);
+		// Step the fourth and final instruction to complete the test
+		runStep();
+		assertEqual(cpu.readProgramCounter(), 0xffffffffU);
+		assertEqual(cpu.readAddrRegister(7U), 0x00800000U);
+	}
+
 public:
 	CRUNCH_VIS testM68k() noexcept : testsuite{}, memoryMap_t<uint32_t, 0x00ffffffU>{}
 	{
@@ -30,6 +76,7 @@ public:
 	void registerTests() final
 	{
 		CXX_TEST(testDecode)
+		CXX_TEST(testBranch)
 	}
 };
 
