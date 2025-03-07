6ac792c7ebe0f89806622a79d6d22e1c97669f7d
dragonmux <git@dragonmux.network>
2025-03-07 20:59:08 +0000

    test/emulator/cpu/testM68k: Built a test for `cmpa` instructions

--- test/libAudio/emulator/cpu/testM68k.cxx
+++ test/libAudio/emulator/cpu/testM68k.cxx
@@ -513,6 +513,53 @@ private:
 		assertEqual(cpu.readAddrRegister(7U), 0x00800000U);
 	}
 
+	void testCMPA()
+	{
+		writeAddress(0x000000U, uint16_t{0xb1fcU});
+		writeAddress(0x000002U, uint32_t{0x0badf00dU}); // cmpa.l #$0badf00d, a0
+		writeAddress(0x000006U, uint16_t{0xb2fcU});
+		writeAddress(0x000008U, uint16_t{0x007fU}); // cmpa.w #$7f, a1
+		writeAddress(0x00000aU, uint16_t{0xb6fcU});
+		writeAddress(0x00000cU, uint16_t{0xaca7U}); // cmpa.w #$aca7, a3
+		writeAddress(0x00000eU, uint16_t{0xb4c1U}); // cmpa.w d1, a2
+		writeAddress(0x000010U, uint16_t{0x4e75U}); // rts to end the test
+		// Set the CPU to execute this sequence
+		cpu.executeFrom(0x00000000U, 0x00800000U);
+		// Set up d1, a0, a1, a2 and a3 to sensible values
+		cpu.writeDataRegister(1U, 0x00000001U);
+		cpu.writeAddrRegister(0U, 0x0badf00dU);
+		cpu.writeAddrRegister(1U, 0x0000000dU);
+		cpu.writeAddrRegister(2U, 0x00008000U);
+		cpu.writeAddrRegister(3U, 0xfffff00dU);
+		// Set the status register to some improbable value that makes it easy to see if it changes
+		cpu.writeStatus(0x001bU);
+		// Validate starting conditions
+		assertEqual(cpu.readProgramCounter(), 0x00000000U);
+		assertEqual(cpu.readAddrRegister(7U), 0x007ffffcU);
+		// Step the first instruction and validate
+		runStep();
+		assertEqual(cpu.readProgramCounter(), 0x00000006U);
+		assertEqual(cpu.readStatus(), 0x0014U);
+		// Set the status register to some other improbable value that makes it easy to see if it changes
+		cpu.writeStatus(0x000fU);
+		// Step the second instruction and validate
+		runStep();
+		assertEqual(cpu.readProgramCounter(), 0x0000000aU);
+		assertEqual(cpu.readStatus(), 0x0009U);
+		// Step the third instruction and validate
+		runStep();
+		assertEqual(cpu.readProgramCounter(), 0x0000000eU);
+		assertEqual(cpu.readStatus(), 0x0000U);
+		// Step the fourth instruction and validate
+		runStep();
+		assertEqual(cpu.readProgramCounter(), 0x00000010U);
+		assertEqual(cpu.readStatus(), 0x0002U);
+		// Step the final instruction to complete the test
+		runStep();
+		assertEqual(cpu.readProgramCounter(), 0xffffffffU);
+		assertEqual(cpu.readAddrRegister(7U), 0x00800000U);
+	}
+
 	void testLEA()
 	{
 		writeAddress(0x000000U, uint16_t{0x43d0U}); // lea (a0), a1
@@ -873,6 +920,7 @@ public:
 		CXX_TEST(testANDI)
 		CXX_TEST(testCLR)
 		CXX_TEST(testCMP)
+		CXX_TEST(testCMPA)
 		CXX_TEST(testLEA)
 		CXX_TEST(testLSL)
 		CXX_TEST(testLSR)
