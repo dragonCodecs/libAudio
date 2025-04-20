ca10239739e5be74196439e8d04e2105e63ebef1
dragonmux <git@dragonmux.network>
2025-04-20 08:19:42 +0100

    test/emulator/cpu/testM68k: Completed the `muls.w` instruction test case

--- test/libAudio/emulator/cpu/testM68k.cxx
+++ test/libAudio/emulator/cpu/testM68k.cxx
@@ -1271,10 +1271,13 @@ private:
 	{
 		writeAddress(0x000000U, uint16_t{0xc1fcU});
 		writeAddress(0x000002U, uint16_t{0x0003U}); // muls.w #3, d0
+		writeAddress(0x000004U, uint16_t{0xc3c0U}); // muls.w d0, d1
+		writeAddress(0x000006U, uint16_t{0x4e75U}); // rts to end the test
 		// Set the CPU to execute this sequence
 		cpu.executeFrom(0x00000000U, 0x00800000U);
-		// Set up d0 to a sensible value
+		// Set up d0 and d1 to sensible values
 		cpu.writeDataRegister(0U, 0x0ca78132U);
+		cpu.writeDataRegister(1U, 0x00010000U);
 		// Validate starting conditions
 		assertEqual(cpu.readProgramCounter(), 0x00000000U);
 		assertEqual(cpu.readAddrRegister(7U), 0x007ffffcU);
@@ -1285,6 +1288,17 @@ private:
 		assertEqual(cpu.readProgramCounter(), 0x00000004U);
 		assertEqual(cpu.readDataRegister(0U), 0xfffe8396U);
 		assertEqual(cpu.readStatus(), 0x0018U);
+		// Set the status register to some other improbable value that makes it easy to see if it changes
+		cpu.writeStatus(0x0001U);
+		// Step the second instruction and validate
+		runStep();
+		assertEqual(cpu.readProgramCounter(), 0x00000006U);
+		assertEqual(cpu.readDataRegister(1U), 0x00000000U);
+		assertEqual(cpu.readStatus(), 0x0004U);
+		// Step the final instruction to complete the test
+		runStep();
+		assertEqual(cpu.readProgramCounter(), 0xffffffffU);
+		assertEqual(cpu.readAddrRegister(7U), 0x00800000U);
 	}
 
 	void testMULU()
