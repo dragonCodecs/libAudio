a98f8bb5eaddf66630d59132772defff4b8a7ac0
dragonmux <git@dragonmux.network>
2025-04-28 09:40:58 +0100

    test/emulator/sound/testSTeDAC: Set up a Microwire transaction

--- test/libAudio/emulator/sound/testSTeDAC.cxx
+++ test/libAudio/emulator/sound/testSTeDAC.cxx
@@ -9,13 +9,42 @@
 
 using m68kMemoryMap_t = memoryMap_t<uint32_t, 0x00ffffffU>;
 
+void writeRegister(peripheral_t<uint32_t> &periph, uint8_t reg, uint8_t value) noexcept
+{
+	periph.writeAddress(reg, {&value, 1U});
+}
+
+void writeRegister(peripheral_t<uint32_t> &periph, uint8_t reg, uint16_t value) noexcept
+{
+	std::array<uint8_t, 2> data{};
+	writeBE(value, data);
+	periph.writeAddress(reg, data);
+}
+
+template<typename T> T readRegister(peripheral_t<uint32_t> &periph, uint8_t reg) noexcept
+{
+	std::array<uint8_t, sizeof(T)> result{};
+	periph.readAddress(reg, result);
+	if constexpr (sizeof(T) == 1)
+		return result[0U];
+	else
+		return readBE<T>(result);
+}
+
 class testSTeDAC final : public testsuite, m68kMemoryMap_t
 {
 	motorola68000_t cpu{*this, 8_MHz};
 	mc68901_t mfp{2457600U, cpu, 6U};
 	steDAC_t dac{50_kHz + 66U, mfp};
 
-	void testRegisterIO()
+	void testMicrowire()
+	{
+		writeRegister(dac, 0x24U, uint16_t{0x0ffeU});
+		assertEqual(readRegister<uint16_t>(dac, 0x24U), 0x0ffeU);
+		writeRegister(dac, 0x22U, uint16_t{0x09d1U});
+	}
+
+	void testDMARegisterIO()
 	{
 		//
 	}
@@ -25,7 +54,8 @@ public:
 
 	void registerTests() final
 	{
-		CXX_TEST(testRegisterIO)
+		CXX_TEST(testMicrowire)
+		CXX_TEST(testDMARegisterIO)
 	}
 };
 
