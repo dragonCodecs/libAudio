554f0ae1045ff746e1c67eb11c19612132b21f6f
dragonmux <git@dragonmux.network>
2025-04-28 09:54:34 +0100

    test/emulator/sound/testSTeDAC: Run a few transactions on the volume register to see it work and react

--- test/libAudio/emulator/sound/testSTeDAC.cxx
+++ test/libAudio/emulator/sound/testSTeDAC.cxx
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // SPDX-FileCopyrightText: 2025 Rachel Mant <git@dragonmux.network>
+#include <cstdint>
 #include <crunch++.h>
+#include <substrate/index_sequence>
 #include "emulator/memoryMap.hxx"
 #include "emulator/cpu/m68k.hxx"
 #include "emulator/timing/mc68901.hxx"
@@ -37,11 +39,46 @@ class testSTeDAC final : public testsuite, m68kMemoryMap_t
 	mc68901_t mfp{2457600U, cpu, 6U};
 	steDAC_t dac{50_kHz + 66U, mfp};
 
+	void runMicrowireCycle(uint16_t mask)
+	{
+		// Microwire cycles take 16 reads of the mask register
+		for (const auto &cycle : substrate::indexSequence_t{16U})
+		{
+			// Compute what the next mask value should be
+			mask = (mask << 1U) | (mask >> 15U);
+			// Read the mask register (which actions the transaction)
+			assertEqual(readRegister<uint16_t>(dac, 0x24U), mask);
+		}
+	}
+
 	void testMicrowire()
 	{
+		// Set up a write to the main volume register
 		writeRegister(dac, 0x24U, uint16_t{0x0ffeU});
 		assertEqual(readRegister<uint16_t>(dac, 0x24U), 0x0ffeU);
+		// Set the volume to 0
+		writeRegister(dac, 0x22U, uint16_t{0x0981U});
+		assertEqual(readRegister<uint16_t>(dac, 0x22U), 0x0981U);
+		// Execute the transaction
+		runMicrowireCycle(0x0ffeU);
+		// Read the main volume to check what it's now set to (should be 0)
+		assertEqual(dac.outputLevel(), 0x00U);
+
+		// Set up another write to the main volume register to set the volume to max (40 internally, 64 externally)
 		writeRegister(dac, 0x22U, uint16_t{0x09d1U});
+		assertEqual(readRegister<uint16_t>(dac, 0x22U), 0x09d1U);
+		// Execute the transaction
+		runMicrowireCycle(0x0ffeU);
+		// Read the main volume to check what it's now set to (should be 40)
+		assertEqual(dac.outputLevel(), 0x40U);
+
+		// Set up another write to the main volume register to over-set the volume to 63
+		writeRegister(dac, 0x22U, uint16_t{0x09ffU});
+		assertEqual(readRegister<uint16_t>(dac, 0x22U), 0x09ffU);
+		// Execute the transaction
+		runMicrowireCycle(0x0ffeU);
+		// Read the main volume to check what it's now set to (should be 64)
+		assertEqual(dac.outputLevel(), 0x40U);
 	}
 
 	void testDMARegisterIO()
